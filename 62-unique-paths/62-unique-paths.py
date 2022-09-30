03332219ef4e7d0a8b71d90559abbc19330eb7ec
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-09-30 14:26:04 -0500

    Time: 46 ms (7.71%), Space: 13.4 MB (45.24%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def uniquePaths(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        # dp mem / bottom up
+        mem = [[False]*n]*m
+        mem[0][0] = 0
+        
+        ## top down: maximum recursion and its not dp (no mem)
+        for i in range(m):
+            for j in range(n):
+                if i==0 or j==0:
+                    mem[i][j] = 1
+                else:
+                    mem[i][j] = mem[i-1][j] + mem[i][j-1]
+                    
+        return mem[m-1][n-1]
+                    
+        
+            
+        
+        
