e2ff24123db9be00d0a9e734764d968ff5ece6b1
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-10-03 10:06:35 -0500

    Time: 98 ms (83.94%), Space: 19.7 MB (13.05%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -4,11 +4,19 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        ans = collections.defaultdict(list) ## note collections.defaultdict(list)
+#         ans = collections.defaultdict(list) ## note collections.defaultdict(list)
+#         for word in strs:
+#             count = [0]*26
+#             for char in word:
+#                 count[ord(char)-ord('a')] += 1
+#             ans[tuple(count)].append(word)
+            
+#         return ans.values()
+        res = defaultdict(list)
         for word in strs:
             count = [0]*26
             for char in word:
-                count[ord(char)-ord('a')] += 1
-            ans[tuple(count)].append(word)
-            
-        return ans.values()
+                count[ord(char) - ord('a')] += 1
+            res[tuple(count)].append(word)
+        
+        return res.values()
