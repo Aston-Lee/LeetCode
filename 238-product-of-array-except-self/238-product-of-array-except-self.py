51fc2b743447874c5772582975edad54ea527522
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-10-03 22:07:45 -0500

    Time: 561 ms (10.75%), Space: 20.8 MB (75.75%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -5,26 +5,48 @@ class Solution(object):
         :rtype: List[int]
         """
         ## prefix postfix no optimize
-        prefix = []
-        pre = 1
-        for n in nums:
-            pre *= n
-            prefix.append(pre)
+#         prefix = []
+#         pre = 1
+#         for n in nums:
+#             pre *= n
+#             prefix.append(pre)
             
-        postfix = []
-        post = 1
-        for n in nums[::-1]:
-            post *= n
-            postfix.append(post)
-        postfix = postfix[::-1]
+#         postfix = []
+#         post = 1
+#         for n in nums[::-1]:
+#             post *= n
+#             postfix.append(post)
+#         postfix = postfix[::-1]
         
-        output = [0]*len(prefix)
-        for i in range(len(prefix)):
+#         output = [0]*len(prefix)
+#         for i in range(len(prefix)):
+#             if i == 0:
+#                 output[i] = 1*postfix[i+1]
+#             elif i == len(prefix)-1:
+#                 output[i] = prefix[i-1] * 1
+#             else:
+#                 output[i] = prefix[i-1] * postfix[i+1]
+#         return output
+        
+        ## prefix postfix optimize
+        output = []
+        res = 1
+        for i in range(len(nums)):
             if i == 0:
-                output[i] = 1*postfix[i+1]
-            elif i == len(prefix)-1:
-                output[i] = prefix[i-1] * 1
+                output.append(1)
             else:
-                output[i] = prefix[i-1] * postfix[i+1]
+                res *= nums[i-1]
+                output.append(res)
+
+        res = 1
+        for j in range(len(nums)-1, -1, -1):  ## start, end_bound(lower), howmany
+            
+            if j == len(nums)-1:
+                output[j] *= 1
+            else:
+                res *= nums[j+1]
+                output[j] *= res
+                
         return output
-        
+                
+            
