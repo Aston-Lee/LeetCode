a53c271a40864eb806d16781013f279466e08b40
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-10-04 13:08:21 -0500

    Time: 169 ms (54.48%), Space: 14.1 MB (87.69%) - LeetHub

---
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        record = {}
+        for i, n in enumerate(numbers):
+            if n in record.keys():
+                return[record[n]+1, i+1]
+            else:
+                record[target - n] = i
+        
