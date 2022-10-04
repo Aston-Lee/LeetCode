5c6dbb357468cb3d3b7236db6ef9afe2e5e00304
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-10-04 13:13:01 -0500

    Time: 228 ms (23.36%), Space: 14.3 MB (40.21%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -5,10 +5,25 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        record = {}
-        for i, n in enumerate(numbers):
-            if n in record.keys():
-                return[record[n], i+1]
+        # # normal approach
+        # record = {}
+        # for i, n in enumerate(numbers):
+        #     if n in record.keys():
+        #         return[record[n], i+1]
+        #     else:
+        #         record[target - n] = i+1
+        
+        # 2 pointer apporach
+        lo = 0
+        hi = len(numbers)-1
+        
+        while(lo!=hi):
+            num = numbers[lo] + numbers[hi]
+            if num == target:
+                return [lo+1, hi+1]
+            elif num > target:
+                hi -= 1
             else:
-                record[target - n] = i+1
+                lo += 1
+        
         
