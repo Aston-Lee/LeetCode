120b7a73e0aa69db1290c956543b33bc48311f94
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-10-18 14:49:57 -0500

    Time: 2302 ms (16.21%), Space: 30.8 MB (20.27%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
@@ -6,16 +6,16 @@ class Solution(object):
         """
         max_val = max(nums)
         count = Counter(nums)
-        taken = []
-        # taken = deque() ## usign deque is the same
+        # taken = []
+        taken = deque() ## usign deque is the same
         
         moves = 0
         for x in range(len(nums) + max_val):
             if count[x] > 1:
                 taken.extend([x]*(count[x]-1))
             elif taken and count[x] == 0:
-                n = taken.pop()
-                # n = taken.popleft()
+                # n = taken.pop()
+                n = taken.popleft()
                 # print(n)
                 moves += x-n
         
