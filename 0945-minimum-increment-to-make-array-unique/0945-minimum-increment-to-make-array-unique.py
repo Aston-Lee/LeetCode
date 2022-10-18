12250d2770988b96b6b551ced90e650699c566bd
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-10-18 14:48:54 -0500

    Time: 1331 ms (63.51%), Space: 31.1 MB (6.76%) - LeetHub

---
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minIncrementForUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_val = max(nums)
+        count = Counter(nums)
+        # taken = []
+        taken = deque()
+        
+        moves = 0
+        for x in range(len(nums) + max_val):
+            if count[x] > 1:
+                taken.extend([x]*(count[x]-1))
+            elif taken and count[x] == 0:
+                n = taken.popleft()
+                print(n)
+                moves += x-n
+        
+        return moves
+        
