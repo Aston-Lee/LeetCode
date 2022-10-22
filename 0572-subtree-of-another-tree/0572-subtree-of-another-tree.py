7b92ecc70a4e1db83aad71f4b362acb8c51bca5d
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-10-21 21:40:26 -0500

    Time: 446 ms (5.06%), Space: 15.1 MB (64.54%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -7,6 +7,13 @@
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         
+#         Time complexity: O(MN)O(MN). For every NN node in the tree, we check if the tree rooted at node is identical to subRoot. This check takes O(M)O(M) time, where MM is the number of nodes in subRoot. Hence, the overall time complexity is O(MN)O(MN).
+
+#         Space complexity: O(M+N)O(M+N).
+
+#         There will be at most NN recursive call to dfs ( or isSubtree). Now, each of these calls will have MM recursive calls to isIdentical. Before calling isIdentical, our call stack has at most O(N)O(N) elements and might increase to O(N + M)O(N+M) during the call. After calling isIdentical, it will be back to at most             O(N)O(N) since all elements made by isIdentical are popped out. Hence, the maximum number of elements in the call stack will be M+NM+N.
+        
+        
         def validate(node, subnode):
             if node == None and subnode:
                 return False
@@ -23,7 +30,6 @@ class Solution:
             
             if node.val == subRoot.val: ## begin traverse
                 found = validate(node, subRoot)
-                print(found)
                 if found == True:
                     return True
             
