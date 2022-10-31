ecf3d6b3297fbfa247f0529d8b720727eb47f002
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-10-31 00:46:54 -0500

    Time: 76 ms (15.08%), Space: 14.2 MB (35.76%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,14 +1,15 @@
-class Solution(object):
-    def subsets(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        n = len(nums)
-        res = []
-        for i in range(2**n, 2**(n+1)):
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ### use mask
+        ### crucial to think of bitmap, and how to successfully generate th bitmap
+        subsets = []
+        for i in range(2**len(nums), 2**(len(nums)+1)):
             bitmap = bin(i)[3:]
-            res.append([ nums[j] for j in range(n) if bitmap[j]=='1'])
-            
-        return res
-            
+            print(bitmap)
+            subset = []
+            for j in range(len(bitmap)):
+                if bitmap[j] == '1':
+                    subset.append(nums[j])
+            subsets.append(subset)
+        
+        return subsets
