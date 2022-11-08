ec481523ed5d92f1b8f03c52f8cf34176f026727
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-11-08 12:39:17 -0600

    Time: 1531 ms (34.20%), Space: 27.9 MB (12.53%) - LeetHub

---
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -0,0 +1,11 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        l, r = 0, len(arr)-1   
+        while(l<r):
+            mid = (l+r)//2
+            if arr[mid] <= arr[mid+1]:
+                l = mid + 1
+            else:
+                r = mid
+
+        return l
