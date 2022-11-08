feafed5252ed01bd6f3bd14f1ab2df185c3f2444
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-11-07 20:14:04 -0600

    Time: 2408 ms (29.87%), Space: 30.2 MB (6.21%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,17 +1,5 @@
-class Solution(object):
-    def dailyTemperatures(self, temperatures):
-        """
-        :type temperatures: List[int]
-        :rtype: List[int]
-        """
-        ## stack, start from back
-        ## logic
-        ## if element is bigger than stack[-1], pop the stack and set res to position difference, and push itself
-        ## if element is <= than stack[-1], push [pos, tmp] into the stack
-        
-        ## edge case 
-        ## position 0, normal
-        ## position -1, result always 0, use the last one to help position -2
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         tmp = temperatures
         
         stack = []
@@ -26,6 +14,3 @@ class Solution(object):
                 stack.append([i,tmp[i]])
         
         return res
-                
-            
-        
