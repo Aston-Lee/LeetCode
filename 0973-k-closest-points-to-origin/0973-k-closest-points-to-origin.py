6c6acc506f9899d2e1048e3b8a8cc220507b256a
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-11-19 14:08:50 -0600

    Time: 838 ms (92.09%), Space: 20.4 MB (62.73%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -2,6 +2,7 @@ class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         
         ## minheap: heapq.heapify
+        ## Time: O(N) Space: O(N)
         minheap = []
         
         for point in points:
@@ -16,7 +17,19 @@ class Solution:
         return res
         
         
-        
         ## followup: try the pushpop, maintaining k size array 
+        minheap = []
         
+        for point in points:
+            length = point[0]**2 + point[1]**2
+            if len(minheap) == k and length < minheap[-1][0]:
+                heapq.heappushpop(minheap, (length, (point)))
+            else:
+                minheap.heappush((length, (point)))
+            
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(minheap)[1])
+        
+        return res
         
