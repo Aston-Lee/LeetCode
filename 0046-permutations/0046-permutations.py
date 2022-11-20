c44bcbb02c995af4b6e80e452479dcdc8cadcdc8
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-11-19 22:09:56 -0600

    Time: 96 ms (11.14%), Space: 14.1 MB (23.97%) - LeetHub

---
+++ 0046-permutations/0046-permutations.py
@@ -0,0 +1,46 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        # @dfs 
+#         n = len(nums)
+#         results = []
+#         def dfs(nums, path, result):
+#             if len(path) == n:
+#                 result.append(path[:])
+#             for i in range(len(nums)):
+#                 dfs(nums[:i]+nums[i+1:n], path+[nums[i]], result)
+                
+#         dfs(nums, [], results)
+#         return results
+        
+        
+# dfs(nums = [1, 2, 3] , path = [] , result = [] )
+# |____ dfs(nums = [2, 3] , path = [1] , result = [] )
+# |      |___dfs(nums = [3] , path = [1, 2] , result = [] )
+# |      |    |___dfs(nums = [] , path = [1, 2, 3] , result = [[1, 2, 3]] ) # added a new permutation to the result
+# |      |___dfs(nums = [2] , path = [1, 3] , result = [[1, 2, 3]] )
+# |           |___dfs(nums = [] , path = [1, 3, 2] , result = [[1, 2, 3], [1, 3, 2]] ) # added a new permutation to the result
+# |____ dfs(nums = [1, 3] , path = [2] , result = [[1, 2, 3], [1, 3, 2]] )
+# |      |___dfs(nums = [3] , path = [2, 1] , result = [[1, 2, 3], [1, 3, 2]] )
+# |      |    |___dfs(nums = [] , path = [2, 1, 3] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3]] ) # added a new permutation to the result
+# |      |___dfs(nums = [1] , path = [2, 3] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3]] )
+# |           |___dfs(nums = [] , path = [2, 3, 1] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1]] ) # added a new permutation to the result
+# |____ dfs(nums = [1, 2] , path = [3] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1]] )
+#        |___dfs(nums = [2] , path = [3, 1] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1]] )
+#        |    |___dfs(nums = [] , path = [3, 1, 2] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2]] ) # added a new permutation to the result
+#        |___dfs(nums = [1] , path = [3, 2] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2]] )
+#             |___dfs(nums = [] , path = [3, 2, 1] , result = [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]] ) # added a new permutation to the result
+
+        res = []
+        n = len(nums)
+        def backtrack(nums, path):
+            if len(path) == n:
+                res.append(path[:])
+                return
+            for i in range(len(nums)):
+                _nums = nums[:i]+nums[i+1:]
+                path.append(nums[i])
+                backtrack(_nums, path)
+                path.pop()
+                
+        backtrack(nums, path=list())
+        return res
