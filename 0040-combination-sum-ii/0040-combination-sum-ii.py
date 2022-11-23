a419f5c63155a490f1c1813694d60c93ace28cf4
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-11-23 11:48:37 -0600

    Time: 159 ms (51.47%), Space: 13.8 MB (93.81%) - LeetHub

---
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        def backtrack(comb, remain, curr, counter, results):
+            if remain == 0 :
+                #  make a deep copy of the current combination
+                #  rather than keeping the reference.
+                results.append(copy.deepcopy(comb))
+                return 
+            elif remain < 0:
+                return
+            
+            for i in range(curr, len(counter)):
+                candidate, freq = counter[i]
+                
+                if freq <= 0:
+                    continue
+                    
+                comb.append(candidate)
+                counter[i] = (candidate, freq-1)
+                backtrack(comb, remain-candidate, i, counter, results)
+                comb.pop()
+                counter[i] = (candidate, freq)
+                    
+        results = []
+        counter = collections.Counter(candidates)
+        counter = [ (c, counter[c]) for c in counter]
+        
+        
+        
+        backtrack( comb = [], remain = target, curr = 0, counter = counter, results = results )
+        
+        return results
+        
