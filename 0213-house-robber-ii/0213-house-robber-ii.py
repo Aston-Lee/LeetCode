81f0d0a637d4becd58e61ff68078578d739db5c9
Aston <76436175+Aston-Lee@users.noreply.github.com>
2022-11-25 15:17:43 -0600

    Time: 36 ms (89.00%), Space: 13.9 MB (72.66%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        def dp(nums):
+            mem = [False] * len(nums)
+            if len(nums) >= 1:
+                mem[0] = nums[0]
+            if len(nums) >= 2:
+                mem[1] = nums[1]
+            if len(nums) == 2:
+                return max(mem[:2])
+            for i in range(2, len(nums)):
+                mem[i] = nums[i] + max(mem[i-2], mem[i-3])
+            if len(mem) >= 2:
+                return max(mem[-1], mem[-2])
+            else:
+                return mem[-1]
+
+        
+        def helper(nums):
+            if len(nums) == 1:
+                return nums[0]
+            else:
+                return max(dp(nums[:-1]), dp(nums[1:]))
+        
+        return helper(nums)
+        
+        
+        
