de77065db352829435d3660fa4b9a02a791840c9
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-12 15:00:16 +0800

    Time: 91 ms (58.07%), Space: 17.2 MB (99.19%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,19 +1,31 @@
 class Solution:
     def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
         
-        ## brute force solution
-        maxEnd = 0
-        for start, end in intervals:
-            maxEnd = max(maxEnd, end)
+#         ## brute force solution
+#         maxEnd = 0
+#         for start, end in intervals:
+#             maxEnd = max(maxEnd, end)
         
-        booked = [False]*maxEnd
-        for start, end in intervals:
-            for i in range(start, end):
-                if booked[i] == True:
-                    return False
-                else:
-                    booked[i] = True
+#         booked = [False]*maxEnd
+#         for start, end in intervals:
+#             for i in range(start, end):
+#                 if booked[i] == True:
+#                     return False
+#                 else:
+#                     booked[i] = True
+#         return True
+
+        sorted_intervals = sorted(intervals)
+        currMaxEnd = 0
+        for start, end in sorted_intervals:
+            if start < currMaxEnd:
+                return False
+            currMaxEnd = max(currMaxEnd, end)
         return True
+            
+    
+    
+        
         
         
 
