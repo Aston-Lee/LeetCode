75672cc1976519f5ebd056fa037878522c0caec6
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-15 10:19:05 +0800

    Time: 481 ms (28.35%), Space: 14.9 MB (10.73%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,23 +1,76 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
+        ## my idea hehe
+        
+        # s = "DEADOBECODEBANC", t = "ABC"
+          
         if len(s) < len(t):
             return ""
-       
-        freqdict = Counter(t)
-        l, r = 0, 0
-        minLen = float('INF')
-        res = ""
-        count = defaultdict(int)
         
-        while r < len(s):
-            if s[r] in freqdict:
-                count[s[r]] += 1
-            while all(count[c] >= freqdict[c] for c in freqdict):
-                if r - l + 1 < minLen:
-                    minLen = r - l + 1
-                    res = s[l:r+1]
-                if s[l] in freqdict:
-                    count[s[l]] -= 1
-                l += 1
-            r += 1
-        return res
+        UpperArr = [0]*26
+        LowerArr = [0]*26
+        tSet = set(t)
+        freqdict = Counter(t)
+        
+        l, r = None, None
+        for i, char in enumerate(s):
+            if char in tSet:
+                l, r = i, i
+                break
+                
+        if l == None: 
+            return ""
+
+        for key, value in freqdict.items():
+            if key.isupper():
+                UpperArr[ord(key)-ord('A')] = value
+            else:
+                LowerArr[ord(key)-ord('a')] = value
+                
+        if s[l].isupper():
+            UpperArr[ord(s[l])-ord('A')] -= 1
+        elif s[l] in tSet and not s[l].isupper():
+            LowerArr[ord(s[l])-ord('a')] -= 1
+        
+                
+        # "DE, ADOBECODEBANC" "ABC"
+        #               l  r
+        
+        minLen = float('INF')
+        tmpres = [None, None]
+        res = ""
+                
+        while l<=len(s) and r <= len(s):
+            # print(s[r], UpperArr)
+            if max(UpperArr) > 0 or max(LowerArr) > 0:
+                r+=1
+                if r == len(s):
+                    break
+                if s[r] in tSet and s[r].isupper():
+                    UpperArr[ord(s[r])-ord('A')] -= 1
+                elif s[r] in tSet and not s[r].isupper():
+                    LowerArr[ord(s[r])-ord('a')] -= 1
+            else:
+                if minLen > r-l+1:
+                    minLen = r-l+1
+                    tmpres = [l,r]
+                
+                # print(s[l])
+                if s[l].isupper():
+                    UpperArr[ord(s[l])-ord('A')] += 1
+                elif s[l] in tSet and not s[l].isupper():
+                    LowerArr[ord(s[l])-ord('a')] += 1
+                l+=1
+                if l == len(s):
+                    break
+                    
+                while l!=len(s) and s[l] not in tSet:
+                    # print("s[l]:", s[l])
+                    l += 1
+                
+                if l == len(s):
+                    break
+        if tmpres == [None, None]:
+            return ""
+        else:
+            return s[tmpres[0]: tmpres[1]+1]
