b4edce62537641b8ca0bf4391fd6c01489b26c84
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-17 05:29:00 +0800

    Time: 12275 ms (24.43%), Space: 78.1 MB (34.82%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,49 +1,50 @@
 class TrieNode:
     def __init__(self):
-        self.children = {}
+        self.child = {} # {char: Node}
         self.isEnd = False
 
 class WordDictionary:
+
     def __init__(self):
         self.root = TrieNode()
-
-    def addWord(self, word: str) -> None:
-        """
-        Add the word to the Trie
-        """
-        cur = self.root
-        for c in word:
-            if c not in cur.children:
-                cur.children[c] = TrieNode()
-            cur = cur.children[c] 
-        cur.isEnd = True 
-
-    def search(self, word: str) -> bool:
-        """
-        Search the Trie for a word, using a DFS.
-        """
-        def dfs(i: int, node: TrieNode) -> bool:
-            """
-            A helper function that uses DFS to search the Trie.
-            """
-            if i == len(word):
-                return node.isEnd
-            
-            c = word[i]
-            if c == '.':
-                for child_node in node.children.values():
-                    if dfs(i+1, child_node) == True:
+        
+    def dfs(self, node, word):
+        for i, char in enumerate(word):
+            if char == ".":
+                if node.child == {}:
+                    return False #waht 
+                for childnode in node.child.values():
+                    if self.dfs(childnode, word[i+1:]):
                         return True
                 return False
-            else:
-                if c in node.children:
-                    return dfs(i+1, node.children[c])
-                else:
-                    return False
-                
-        return dfs(0, self.root)
+            elif char not in node.child.keys():
+                return False
+            node = node.child[char]
+        return node.isEnd 
+        
 
+    def addWord(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.child.keys():
+                node.child[char] = TrieNode()
+            node = node.child[char]
+        node.isEnd = True
+        
 
+    def search(self, word: str) -> bool:
+        node = self.root
+        for i, char in enumerate(word):
+            if char == ".":
+                for childnode in node.child.values():
+                    if self.dfs(childnode, word[i+1:]):
+                        return True
+                return False
+            elif char not in node.child.keys():
+                return False
+            node = node.child[char]
+        return node.isEnd
+            
 
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
