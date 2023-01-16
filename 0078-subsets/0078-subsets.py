bbad24541a80d8e97b3aba308094108ad9c8317d
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-17 07:28:57 +0800

    Time: 45 ms (48.48%), Space: 14.1 MB (34.30%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -2,15 +2,29 @@ class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         
         ## generate all masks
-        n = len(nums)
-        res = []
-        for i in range(2**n, 2**(n+1)):
-            mask = bin(i)[3:]
-            tmp_res = []
-            for j in range(len(nums)):
-                if mask[j] == '1':
-                    tmp_res.append(nums[j])
-            res.append(tmp_res)
+#         n = len(nums)
+#         res = []
+#         for i in range(2**n, 2**(n+1)):
+#             mask = bin(i)[3:]
+#             tmp_res = []
+#             for j in range(len(nums)):
+#                 if mask[j] == '1':
+#                     tmp_res.append(nums[j])
+#             res.append(tmp_res)
 
             
-        return res
+#         return res
+    
+        n = len(nums)
+        Res = []
+        for i in range(2**n, 2**(n+1)):
+            tmpRes = []
+            mask = bin(i)[3:]
+            for j in range(n):
+                if mask[j] == '1':
+                    tmpRes.append(nums[j])
+            tmpRes = sorted(tmpRes)
+            if tmpRes not in Res:
+                Res.append(tmpRes[:])
+
+        return Res
