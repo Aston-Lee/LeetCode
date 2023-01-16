1b4c0ddb2034ee6b51cecad4f586d6af84d542a5
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-17 07:16:10 +0800

    Time: 69 ms (70.29%), Space: 13.9 MB (93.09%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,22 +1,35 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
-        Res = []
-        def backtrack(candidates, target, tmpRes, start ):
-            if target == 0:
-                # The [:] slice notation is used to create a new list that is a copy of the original list, which can be modified without affecting the original list.
-                Res.append(tmpRes[:]) 
-            
-            if target <= 0:
-                return 
-            
-            for i in range(start, len(candidates)):
-                tmpRes.append(candidates[i])
-                backtrack(candidates, target-candidates[i] , tmpRes, i )
-                tmpRes.pop()
-            return 
-
-        backtrack(candidates, target, [], 0)
-        return Res
+        output = []
         
+        # val = target - currvalue
+        
+        def backtrack(val, combination, start ):
+            if val == 0:
+                output.append(combination[:])
+                return
+            elif val < 0:
+                return 
+            else:
+                for i in range(start, len(candidates)):
+                    combination.append(candidates[i])
+                    backtrack(val-candidates[i], combination, i)
+                    combination.pop()
+        
+        backtrack(target, [], 0)
+        return output
 
+    
+        # candidates = [2,3,6,7], target = 7
+        # backtrack(7, [])
+        #     backtrack(5, [2])    
+        #         backtrack(3, [2,2]) 
+        #             backtrack(1, [2,2,2]) 
+        #                 backtrack(-1, [2,2,2,2]) return
+        #                 backtrack(-2, [2,2,2,3]) return
+        #                     ..
+        #             backtrack(0, [2,2,3]) -> output.append([2,2,3]) -> output = [[2,2,3]]
+        #             backtrack(-3, [2,2,6]) return
+        #             ..
+        #         backtrack(2, [2,3]) 
