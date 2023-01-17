3f7517bf5e04b13704ecf0f8968cd808a80c6ae5
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-18 04:48:38 +0800

    Time: 37 ms (61.96%), Space: 13.8 MB (96.60%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.py
+++ 1046-last-stone-weight/1046-last-stone-weight.py
@@ -1,14 +1,18 @@
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
+        ## smallest or largest -> minheap maxheap
+        
         ## maxheap
         stones = [-s for s in stones]
         heapq.heapify(stones)
 
         while( len(stones)>1 ):
-            n1 = heapq.heappop(stones)
+            ## regular pop will fail since it does not pop the smalleset 
+            n1 = heapq.heappop(stones) 
+            ## heappop pop the smallest
             n2 = heapq.heappop(stones)
             if n1!=n2:
                 heapq.heappush(stones, n1-n2)
-                heapq.heapify(stones)
+                # heapq.heapify(stones)
                 
         return -stones[0] if stones else 0
