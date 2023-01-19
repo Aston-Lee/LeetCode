6c45a2acb51e36bf497d43dfc235c4711d96497f
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-20 05:00:02 +0800

    Time: 25 ms (98.21%), Space: 13.9 MB (66.47%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,27 +1,23 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
+        if len(nums) == 0:
+            return 0
         if len(nums) == 1:
             return nums[0]
-        
-        num1 = nums[:-1]
-        num2 = nums[1:]
-        
-        def dpfunction(arr) -> int: ##return the last of dp, which is the larget of the result
-            dp = [0]*(len(arr)+1) ##1-based 
-            ## note that the arr we passed in is a 0-based 
-            for i in range(len(arr)+1):
-                if i < 3:
-                    dp[i] = arr[i-1]
-                elif i == 3:
-                    dp[i] = max(dp[1]+arr[2], dp[2])
-                else: # i>=4
-                    dp[i] = max(dp[i-2], dp[i-3]) + arr[i-1]
-                print(dp)
-            print("end if dp", dp)
-            return max(dp[-1],dp[-2])
-            
-        return max(dpfunction(num1), dpfunction(num2))
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        dp1 = [0] * (len(nums))
+        dp1[0] = nums[0]
+        dp1[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp1[i] = max(dp1[i-1], dp1[i-2] + nums[i])
+        dp2 = [0] * (len(nums))
+        dp2[0] = 0
+        dp2[1] = nums[1]
+        for i in range(2, len(nums)):
+            dp2[i] = max(dp2[i-1], dp2[i-2] + nums[i])
+        return max(dp1[-2], dp2[-1])
             
             
             
