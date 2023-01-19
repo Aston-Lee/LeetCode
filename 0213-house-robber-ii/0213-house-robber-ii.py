e107c24f1755c54058a48f6917b3d09d385dfb61
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-20 04:53:10 +0800

    Time: 35 ms (72.38%), Space: 14 MB (6.56%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,32 +1,27 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        # cant robe geginning and the last
-        # so seperate two proportion and do regular dp
         
+        if len(nums) == 1:
+            return nums[0]
         
-        def dp(nums):
-            mem = [False] * len(nums)
-            if len(nums) >= 1:
-                mem[0] = nums[0]
-            if len(nums) >= 2:
-                mem[1] = nums[1]
-            if len(nums) == 2:
-                return max(mem[:2])
-            for i in range(2, len(nums)):
-                mem[i] = nums[i] + max(mem[i-2], mem[i-3])
-            if len(mem) >= 2:
-                return max(mem[-1], mem[-2])
-            else:
-                return mem[-1]
-
+        num1 = nums[:-1]
+        num2 = nums[1:]
         
-        def helper(nums):
-            if len(nums) == 1:
-                return nums[0]
-            else:
-                return max(dp(nums[:-1]), dp(nums[1:]))
-        
-        return helper(nums)
-        
-        
-        
+        def dpfunction(arr) -> int: ##return the last of dp, which is the larget of the result
+            dp = [0]*(len(arr)+1) ##1-based 
+            ## note that the arr we passed in is a 0-based 
+            for i in range(len(arr)+1):
+                if i < 3:
+                    dp[i] = arr[i-1]
+                elif i == 3:
+                    dp[i] = max(dp[1]+arr[2], dp[2])
+                else: # i>=4
+                    dp[i] = max(dp[i-2], dp[i-3]) + arr[i-1]
+                print(dp)
+            print("end if dp", dp)
+            return max(dp[-1],dp[-2])
+            
+        return max(dpfunction(num1), dpfunction(num2))
+            
+            
+            
