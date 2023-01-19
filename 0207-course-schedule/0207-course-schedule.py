b3da5809dcf4410e8a80a3ebc29367c6ec11597b
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-19 14:46:11 +0800

    Time: 102 ms (79.47%), Space: 15.3 MB (96.15%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,31 +1,18 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        # Create the prerequisites map 
-        preMap = defaultdict(list)
-        for crs, pre in prerequisites:
-            preMap[crs].append(pre)
-        
-        def dfs(crsNum, seen) -> bool:
-            # If there are no more prerequisites, return True
-            if preMap[crsNum] == []:
-                return True
-            # If the course has already been seen, there is a cycle so return False
-            if crsNum in seen:
-                return False
-            seen.add(crsNum)
-            # Check the prerequisites of the current course
-            for preNum in preMap[crsNum]:
-                if not dfs(preNum, seen):
-                    return False
-            # Once all the prerequisites have been visited, remove the course from the seen set
-            seen.remove(crsNum)
-            # Mark the current course as finished traversing
-            preMap[crsNum] = []
-            return True
-        
-        # Check each course
-        for i in range(numCourses):
-            seen = set()
-            if not dfs(i, seen):
-                return False
-        return True
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        for x, y in prerequisites:
+            graph[y].append(x)
+            indegree[x] += 1
+
+        queue = [i for i in range(numCourses) if indegree[i] == 0]
+        cnt = 0
+        while queue:
+            cnt += 1
+            node = queue.pop(0)
+            for neighbor in graph[node]:
+                indegree[neighbor] -= 1
+                if indegree[neighbor] == 0:
+                    queue.append(neighbor)
+        return cnt == numCourses
