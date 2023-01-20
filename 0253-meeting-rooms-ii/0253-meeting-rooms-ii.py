5f03f1ea096fc2e5560e3cd4dd9ab3d6356edb4c
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-20 14:59:21 +0800

    Time: 1398 ms (5.02%), Space: 17.8 MB (21.87%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,6 +1,7 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
+        ## brute force O(n^2) / 
         startarr , endarr = [], []
         
         for start, end in intervals:
@@ -14,6 +15,8 @@ class Solution:
         maxroom, roomnum = 0, 0
         
         for i in range(sortedEnd[0]+1):
+            if not sortedStart:
+                break
             while sortedStart and i >= sortedStart[-1]:
                 roomnum += 1
                 sortedStart.pop()
