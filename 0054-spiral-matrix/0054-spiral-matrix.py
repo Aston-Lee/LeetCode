b9838652daf942cfd1adde03ceec5c9a821ba001
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-21 08:39:58 +0800

    Time: 41 ms (42.56%), Space: 13.9 MB (78.56%) - LeetHub

---
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -0,0 +1,33 @@
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        
+        direction  = {
+            (0,1) : (1,0),
+            (1,0) : (0,-1),
+            (0,-1) : (-1,0),
+            (-1,0) : (0,1)
+        } 
+        seen = set() #store the pos
+        
+        m = len(matrix)
+        n = len(matrix[0])
+        
+        q = deque()
+        q.append((0, 0))
+        di, dj = 0,1
+        output = []
+        
+        while q:
+            if len(output) == m*n:
+                break
+            i, j = q.popleft()
+            seen.add((i,j))
+            output.append(matrix[i][j])
+            if 0<=i+di<m and 0<=j+dj<n and (i+di, j+dj) not in seen:
+                q.append((i+di,j+dj))
+            else:
+                di, dj = direction[(di, dj)]
+                q.append((i+di,j+dj))
+                # print(di, dj)
+        
+        return output
