4ffbab05d8bd7bbdf1b500970ec5e58c0f790e42
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-25 04:29:18 +0800

    Time: 3602 ms (21.43%), Space: 18.4 MB (22.62%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,23 +1,27 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        
+        output = []
         nums.sort()
         
-        for i, a in enumerate(nums):
-            if i > 0 and a == nums[i-1]:
+        [1,1,1]
+        
+        for i in range(len(nums)-2):
+            if i>0 and nums[i] == nums[i-1]:
                 continue
-            
             l, r = i+1, len(nums)-1
-            while(l<r):
-                threesum = a + nums[l] + nums[r]
-                if threesum > 0:
-                    r -= 1
-                elif threesum < 0:
-                    l += 1
-                else:
-                    res.append([a, nums[l], nums[r]])
-                    l += 1
-                    while nums[l] == nums[l-1] and l < r:
-                        l += 1
-                        
-        return res 
+            while l < r : 
+                if nums[i] + nums[l] + nums[r] == 0:
+                    output.append([nums[i], nums[l], nums[r]])
+                    l+=1
+                    while l<r and nums[l] == nums[l-1]:
+                        l+=1
+                elif nums[i] + nums[l] + nums[r] < 0:
+                    l+=1
+                    while l<r and nums[l] == nums[l-1]:
+                        l+=1
+                elif nums[i] + nums[l] + nums[r] > 0:
+                    r-=1
+                    while r>0 and nums[r] == nums[r+1]:
+                        r-=1
+        return output
