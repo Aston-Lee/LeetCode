ed18eca13510f0877c73615a6d6300e19e009699
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-24 10:46:22 +0800

    Time: 144 ms (61.23%), Space: 16.6 MB (93.54%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,12 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        seen = []
-        for n in nums:
-            if n in seen:
-                seen.pop(indexOf(seen,n))
-            else:
-                seen.append(n)
-        return seen[0]
+        hashtable = defaultdict(int)
+        for i in nums:
+            hashtable[i] += 1
+            
+        for i in hashtable:
+            if hashtable[i] == 1:
+                return i
+        
                 
         
         
