42173e018d012070fd63a8b291f55185f16cff8b
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-25 06:55:35 +0800

    Time: 766 ms (92.94%), Space: 22.8 MB (16.07%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,53 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        # @leftmost rightmost -> is used for rain, not day trade
+        
+#         # two pointer?
+#         least = 1000000  # by not using max() its so much faster 
+#         value = 0
+#         maxvalue =0
+        
+#         for i in prices:
+#             if least > i:
+#                 least = i
+#                 value = 0
+#             else:
+#                 value = i - least
+#                 if maxvalue < value:
+#                     maxvalue = value
+                    
+#         return maxvalue
+
+        # # @2 pointer Time:O(n) Space:O(1)
+        # l, r = 0, 1
+        # maxP = 0
+        # while r<len(prices):
+        #     if prices[l] < prices[r]:
+        #         maxP = max( maxP, prices[r] - prices[l] )
+        #     else:
+        #         l = r
+        #     r += 1 
+        # return maxP
+        
+        currmin, allmax = float('inf'), -float('inf')
+        
+        for p in prices:
+            if p < currmin:
+                currmin = p
+            allmax = max(allmax, p-currmin)
+            
+        return allmax
+            
+        
+        
+        
+        
+        
+        
+        
+        
+                
