856f32ab3142534b817d88e9624be850e0d5bc46
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-25 08:46:26 +0800

    Time: 53 ms (24.81%), Space: 13.9 MB (17.40%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,41 +1,35 @@
 class Solution:
     def decodeString(self, s: str) -> str:
-
+        
         num = 0
         stack = []
         for char in s:
             if char.isdigit():
                 num *= 10
                 num += int(char)
-            elif char == '[': ## char is '[', ']'
+            elif char == '[':
                 stack.append(num)
+                stack.append('[')
                 num = 0
-                stack.append(char)
-                
             elif char == ']':
-
                 tmpStr = ""
-                while(stack[-1] != '['):
-                    tmpStr += stack.pop()[::-1]
-                   
+                while stack[-1] != '[':
+                    tmpStr += stack.pop()
                 stack.pop()
                 times = stack.pop()
-                tmpStr *= times #caca
-                stack.append(tmpStr[::-1])
-                # print(stack)
-                
+                stack.append(times*tmpStr)
+                # times = 0
+                # tmpStr = ""
             else:
                 stack.append(char)
-        
-        res = ""
-        for item in stack:
-            res += item
-        
-        return res
                 
-        # "3[a]2[bc]"
+        res = ""
+        while stack:
+            res += stack.pop()
         
-        stack:   aaa
+        return res[::-1]
+            
+            
             
                   
                     
