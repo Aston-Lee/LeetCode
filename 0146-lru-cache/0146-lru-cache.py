d6f451c226814d9ba91589c84404cac66682c1d2
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-25 09:44:27 +0800

    Time: 922 ms (63.35%), Space: 76.5 MB (23.99%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,25 +1,49 @@
-from collections import OrderedDict
-class LRUCache(OrderedDict):
-    ## usign OrderedDict for LRU problem makes it easy
-    def __init__(self, capacity: int):
-        self.cache = OrderedDict()
-        self.capacity = capacity
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
-    def get(self, key: int) -> int:
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def get(self, key):
         if key not in self.cache:
             return -1
-        self.cache.move_to_end(key)
-        return self.cache[key]
+        node = self.cache[key]
+        self._move_to_head(node)
+        return node.value
 
-    def put(self, key: int, value: int) -> None:
+    def put(self, key, value):
         if key in self.cache:
-            self.cache.move_to_end(key)
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(last = False)
+            node = self.cache[key]
+            node.value = value
+            self._move_to_head(node)
+        else:
+            if len(self.cache) == self.capacity:
+                del self.cache[self.tail.prev.key]
+                self._remove(self.tail.prev)
+            node = Node(key, value)
+            self.cache[key] = node
+            self._add(node)
 
-            
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+    def _move_to_head(self, node):
+        self._remove(node)
+        self._add(node)
+
+    def _remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+
+    def _add(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
