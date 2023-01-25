b7f4ce67ef6412dc48239d6eb0d625edd284af69
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-25 14:22:39 +0800

    Time: 3101 ms (5.01%), Space: 19.3 MB (27.46%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
+        arr = [gas[i]-cost[i] for i in range(len(gas))]
+        if sum(arr) < 0:
+            return -1
+        else:
+            total, remain = 0, 0
+            start = 0
+            for i in range(len(arr)):
+                total += arr[i]
+                remain += arr[i]
+                if remain < 0:
+                    remain = 0
+                    start = i+1
+            return start 
+                
+                
+                # if arr[i] >= 0:
+                #     balance = arr[i]
+                #     dst = i
+                #     pos = i+1
+                #     if pos == len(gas):
+                #         pos = 0
+                #     while pos != dst:
+                #         balance += arr[pos]
+                #         if balance < 0:
+                #             break
+                #         pos += 1
+                #         if pos == len(gas):
+                #             pos = 0
+                #     if pos == dst:
+                #         return i
