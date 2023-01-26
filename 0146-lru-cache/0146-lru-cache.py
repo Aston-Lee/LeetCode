6d6ccc6e0a938e180eb58ab5c759a18465afbb31
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-26 11:20:31 +0800

    Time: 906 ms (68.98%), Space: 76.5 MB (31.63%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,22 +1,58 @@
-class LRUCache:
-    ## OrderedDict solution
-    def __init__(self, capacity: int):
-        self.cache = OrderedDict()
-        self.capacity = capacity
+## Double Link List Version
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}
+        self.head = Node(0,0)
+        self.tail = Node(0,0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        
     def get(self, key: int) -> int:
-        if key not in self.cache:
+        if key in self.cache:
+            node = self.cache[key]
+            self._move_to_head(node)
+            return node.value
+        else:
             return -1
-        self.cache.move_to_end(key)
-        return self.cache[key]
-    
+
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.cache.move_to_end(key)
-        self.cache[key] = value
-        if len(self.cache) > self.capacity:
-            self.cache.popitem(last = False)
-        return self.cache[key]
+            node = self.cache[key]
+            node.value = value
+            self._move_to_head(node)
+        else:
+            if len(self.cache) == self.capacity:
+                del self.cache[self.tail.prev.key]
+                self._remove(self.tail.prev)
+            node = Node(key, value)
+            self.cache[key] = node
+            self._add(node)
+            
+    def _move_to_head(self, node):
+        self._remove(node)
+        self._add(node)
+            
+    def _add(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+        
+    def _remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+        
+        
+
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
