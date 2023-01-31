155cf485edce7de8a8d2019a4499369652487c04
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-01-31 10:25:17 +0800

    Time: 216 ms (67.04%), Space: 24.4 MB (18.24%) - LeetHub

---
+++ 0721-accounts-merge/0721-accounts-merge.py
@@ -0,0 +1,35 @@
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        
+        ## build the adj list
+        emailMap = defaultdict(list)
+        for i, (_, *account) in enumerate(accounts):
+            for j in range(len(account)):
+                email = account[j]
+                emailMap[email].append(i)
+                
+        ## DFS traverse account
+        visitedAccounts = [False] * len(accounts)
+        def dfs(i, emails):
+            if visitedAccounts[i]:
+                return
+            visitedAccounts[i] = True
+            for j in range(1, len(accounts[i])):
+                email = accounts[i][j]
+                emails.add(email)
+                for neighbor in emailMap[email]:
+                    dfs(neighbor, emails)
+        
+        ## Main
+        res = []
+        for i, account in enumerate(accounts):
+            if visitedAccounts[i]:
+                continue
+            name, emails = account[0], set()
+            dfs(i, emails)
+            res.append([name] + sorted(emails))
+        return res
+        
+        
+        
+        
