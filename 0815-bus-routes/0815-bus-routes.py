b7871f3278d23b49ddc95f2107b5c412ba62a806
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-02-02 09:48:50 +0800

    Time: 417 ms (92.72%), Space: 34.3 MB (66.22%) - LeetHub

--- 0815-bus-routes/0815-bus-routes.py
+++ 0815-bus-routes/0815-bus-routes.py
@@ -1,38 +1,7 @@
 class Solution:
     def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
         
-#         ## 1. build a adj mapping for all stops 
-#         adjMap  = defaultdict(list)
-#         for route in routes:
-#             for j in range(len(route)):
-#                 start = route[j]
-#                 others = route[:j] + route[j+1:]
-#                 adjMap[start].extend(others)
-        
-#         # print(adjMap)
-        
-#         ## 2. bfs traverse 
-#         q = deque()
-#         q.append(source)
-        
-#         seen = set()
-#         count = 0
-#         found = False
-        
-#         while q:
-#             count += 1
-#             for _ in range(len(q)):
-#                 src = q.pop()
-#                 if src in seen:
-#                     continue
-#                 seen.add(src)
-#                 for dst in adjMap[src]:
-#                     if dst == target:
-#                         return count
-#                     q.append(dst)
-#             # print(q)
 
-#         return -1
 
         adjMap  = defaultdict(list)
         for i, route in enumerate(routes):
