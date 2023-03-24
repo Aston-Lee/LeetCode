2f3b6a9cbf537ab54c92f8bbc49c4802a728c3a3
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-03-24 12:34:52 -0500

    Time: 116 ms (80.25%), Space: 14 MB (12.27%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -0,0 +1,41 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # ## 2 pointer 
+        # l, r = 0, 0
+        # freqdict = {} 
+        # maxlen = 0
+        # addFlag = True 
+        # while r != len(s):
+        #     if addFlag == True:
+        #         freqdict[s[r]] = freqdict.get(s[r], 0) + 1
+        #     length = r - l + 1 
+        #     if length - max(freqdict.values()) <= k:
+        #         addFlag = True
+        #         maxlen = max(maxlen, length)
+        #         r += 1
+        #     else:
+        #         addFlag = False
+        #         freqdict[s[l]] -= 1
+        #         l += 1
+        # return maxlen
+        
+        l = 0 
+        freq = defaultdict(int)
+        maxlen = 0
+        for r in range(len(s)):
+            # if s[r] in freq.keys():
+            #     freq[s[r]] +=1
+            # else:
+            #     freq[s[r]] = 1
+            freq[s[r]] +=1
+        
+            currentlen = r - l + 1
+            if currentlen - max(freq.values()) <= k :
+                maxlen = max(maxlen, currentlen)
+            else:
+                freq[s[l]] -= 1
+                l += 1
+            # print(freq)
+            
+        return maxlen
+        
