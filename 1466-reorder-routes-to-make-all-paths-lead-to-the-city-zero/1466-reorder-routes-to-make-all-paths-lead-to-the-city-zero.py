a875af9ceb7d8f09e99e895633b48c0969a50a47
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-03-25 23:48:30 -0500

    Time: 1609 ms (5.04%), Space: 49.2 MB (59.43%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        adjList = defaultdict(set)
+        for u, v in connections:
+            adjList[u].add((v, True))
+            adjList[v].add((u, False))
+        
+        print(adjList)
+        
+        queue = deque([0])
+        visited = set([0])
+        
+        changeEdges = 0
+        while queue:
+            node = queue.popleft()
+            
+            for neighbor, isConnection in adjList[node]:
+                if neighbor not in visited:
+                    changeEdges += isConnection
+                    queue.append(neighbor)
+                    visited.add(neighbor)
+                    
+        return changeEdges
