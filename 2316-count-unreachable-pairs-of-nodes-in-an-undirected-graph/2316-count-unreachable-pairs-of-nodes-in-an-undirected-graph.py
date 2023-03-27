b8eee299106d26244d570a9a42b499edd07acff7
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-03-27 15:45:40 -0500

    Time: 2190 ms (57.83%), Space: 75.3 MB (77.09%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -1,27 +1,3 @@
-# class UnionFind:
-#     def __init__(self, size):
-#         self.parent = list(range(size))
-#         self.rank = [0] * size
-
-#     def find(self, x):
-#         if self.parent[x] != x:
-#             self.parent[x] = self.find(self.parent[x])
-#         return self.parent[x]
-
-#     def union_set(self, x, y):
-#         xset = self.find(x)
-#         yset = self.find(y)
-#         if xset == yset:
-#             return
-#         elif self.rank[xset] < self.rank[yset]:
-#             self.parent[xset] = yset
-#         elif self.rank[xset] > self.rank[yset]:
-#             self.parent[yset] = xset
-#         else:
-#             self.parent[yset] = xset
-#             self.rank[xset] += 1
-
-
 class Solution:
     def countPairs(self, n: int, edges: List[List[int]]) -> int:
         
