31e89c6539cc7544bcdfbf4a787772571286f97f
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-03-27 15:42:37 -0500

    Time: 2263 ms (32.37%), Space: 75.4 MB (65.13%) - LeetHub

--- 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -1,42 +1,58 @@
-class UnionFind:
-    def __init__(self, size):
-        self.parent = list(range(size))
-        self.rank = [0] * size
+# class UnionFind:
+#     def __init__(self, size):
+#         self.parent = list(range(size))
+#         self.rank = [0] * size
 
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
+#     def find(self, x):
+#         if self.parent[x] != x:
+#             self.parent[x] = self.find(self.parent[x])
+#         return self.parent[x]
 
-    def union_set(self, x, y):
-        xset = self.find(x)
-        yset = self.find(y)
-        if xset == yset:
-            return
-        elif self.rank[xset] < self.rank[yset]:
-            self.parent[xset] = yset
-        elif self.rank[xset] > self.rank[yset]:
-            self.parent[yset] = xset
-        else:
-            self.parent[yset] = xset
-            self.rank[xset] += 1
+#     def union_set(self, x, y):
+#         xset = self.find(x)
+#         yset = self.find(y)
+#         if xset == yset:
+#             return
+#         elif self.rank[xset] < self.rank[yset]:
+#             self.parent[xset] = yset
+#         elif self.rank[xset] > self.rank[yset]:
+#             self.parent[yset] = xset
+#         else:
+#             self.parent[yset] = xset
+#             self.rank[xset] += 1
 
 
 class Solution:
     def countPairs(self, n: int, edges: List[List[int]]) -> int:
-        # create a new UnionFind object with n elements
-        dsu = UnionFind(n)
         
-        # merge connected components of the graph
+        parent = list(range(n))
+        rank = [0] * n
+        
+        def find(node):
+            if node != parent[node]:
+                parent[node] = find(parent[node])
+            return parent[node]
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            if p1 == p2:
+                return
+            elif rank[p1] < rank[p2]:
+                parent[p1] = p2
+            elif rank[p1] > rank[p2]:
+                parent[p2] = p1
+            else:
+                parent[p2] = p1
+                rank[p1] += 1
+        
+        
         for edge in edges:
-            dsu.union_set(edge[0], edge[1])
+            union(edge[0], edge[1])
         
-        # count the size of each component
         componentSize = {}
         for i in range(n):
-            componentSize[dsu.find(i)] = componentSize.get(dsu.find(i), 0) + 1
+            componentSize[find(i)] = componentSize.get(find(i), 0) + 1
         
-        # calculate the number of pairs of nodes in different components
         numberOfPaths = 0
         remainingNodes = n
         for component in componentSize.values():
