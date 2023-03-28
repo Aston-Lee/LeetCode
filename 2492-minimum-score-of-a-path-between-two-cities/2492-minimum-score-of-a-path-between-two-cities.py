620c98a360d5cd0081ac843b36ebf81300bcf373
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-03-27 23:13:36 -0500

    Time: 1913 ms (29.33%), Space: 70 MB (60.64%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,4 +1,18 @@
 class Solution:
+    
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        adj = [[] for _ in range(n + 1)]
+        for road in roads:
+            adj[road[0]].append((road[1], road[2]))
+            adj[road[1]].append((road[0], road[2]))
+        print(adj)
+
+        visit = [False] * (n + 1)
+        answer = [float(inf)]
+        self.dfs(1, adj, visit, answer)
+        return answer[0]
+        
+        
     def dfs(self, node: int, adj: List[List[Tuple[int, int]]], visit: List[bool], answer: List[int]) -> None:
         
         visit[node] = True
@@ -7,14 +21,6 @@ class Solution:
             if not visit[edge[0]]:
                 self.dfs(edge[0], adj, visit, answer)
 
-    def minScore(self, n: int, roads: List[List[int]]) -> int:
-        adj = [[] for _ in range(n + 1)]
-        for road in roads:
-            adj[road[0]].append((road[1], road[2]))
-            adj[road[1]].append((road[0], road[2]))
+    
 
-        visit = [False] * (n + 1)
-        answer = [sys.maxsize]
-        self.dfs(1, adj, visit, answer)
-
-        return answer[0]
+        
