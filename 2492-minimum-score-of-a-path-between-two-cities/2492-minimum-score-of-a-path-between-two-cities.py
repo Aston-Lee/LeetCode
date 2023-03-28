4daf1f9e51d7a41e31a636863834bc165dbf38f0
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-03-27 23:05:20 -0500

    Time: 1765 ms (46.40%), Space: 63.7 MB (78.07%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,20 @@
+class Solution:
+    def dfs(self, node: int, adj: List[List[Tuple[int, int]]], visit: List[bool], answer: List[int]) -> None:
+        
+        visit[node] = True
+        for edge in adj[node]:
+            answer[0] = min(answer[0], edge[1])
+            if not visit[edge[0]]:
+                self.dfs(edge[0], adj, visit, answer)
+
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        adj = [[] for _ in range(n + 1)]
+        for road in roads:
+            adj[road[0]].append((road[1], road[2]))
+            adj[road[1]].append((road[0], road[2]))
+
+        visit = [False] * (n + 1)
+        answer = [sys.maxsize]
+        self.dfs(1, adj, visit, answer)
+
+        return answer[0]
