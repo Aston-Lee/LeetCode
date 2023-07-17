016ed25de249e66a27c01f8cc17b0e6fdc18ffa1
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-07-17 12:17:48 -0500

    Time: 3347 ms (56.64%), Space: 16.7 MB (72.54%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.py
@@ -1,12 +1,14 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         
-        ## dp solution O(n^2)/ O(n)
-        dp = [1 for _ in range(len(nums))]
+#         7 7 7 7 7 7 7
+#         1 1 1 1 1 1 1
+#           i
+#         j
+        
+        LIS = [1]*len(nums)
         for i in range(1, len(nums)):
-            for j in range(0,i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j]+1)
-        
-        return max(dp)
-        
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    LIS[i] = max(LIS[j]+1, LIS[i])
+        return max(LIS)
