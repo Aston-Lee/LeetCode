99b92bebcdce4782ba00737101e39f5f0e3a9793
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-07-21 11:28:54 -0500

    Time: 141 ms (63.22%), Space: 17.5 MB (50.45%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -1,27 +1,21 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         
-        # BFS, check rotten orange 
-        q = deque()
-        q.append(0)
-        seen = set() # record index
-        jump = 0
-        while q:
-            jump += 1
-            for i in range(len(q)):
-                pos = q.popleft()
-                if pos in seen:
-                    continue
-                seen.add(pos)
-                if pos == len(nums)-1:
-                    return jump-1
-                n = nums[pos]
-                for i in range(1,n+1):
-                    if pos+i == len(nums):
-                        break
-                    q.append(pos+i)
-            
-
+        l, r = 0, 0
+        res = 0
+        
+        while r < (len(nums)-1):
+            maxJump = 0
+            for i in range(l, r+1):
+                maxJump = max(maxJump, i + nums[i])
+            l = r + 1
+            r = maxJump
+            res += 1
+        
+        return res
+        
+        
+       
                     
         
          
