d21e588ae9a3287586eb166b0f0b294f5bf4f042
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-09-17 15:37:32 -0500

    Time: 55 ms (84.02%), Space: 16.4 MB (75.15%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,21 +1,36 @@
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
         
+#         start = 0
+#         charset = set()
+#         maxLength = 0
+#         # i = end
+#         for i, char in enumerate (s):
+#             if char not in charset:
+#                 charset.add(char)
+#                 maxLength = max(maxLength, len(charset))
+#             else:
+#                 while start <= i and s[start] != char:
+#                     charset.remove(s[start])
+#                     start += 1
+#                 if s[start] == char:
+#                     charset.remove(s[start])
+#                     start += 1
+#                 charset.add(char)
+#         maxLength = max(maxLength, len(charset))
+#         return maxLength
+    
+    
+class Solution:    
+    def lengthOfLongestSubstring(self, s: str) -> int:
         start = 0
-        charset = set()
         maxLength = 0
-        # i = end
-        for i, char in enumerate (s):
-            if char not in charset:
-                charset.add(char)
-                maxLength = max(maxLength, len(charset))
-            else:
-                while start <= i and s[start] != char:
-                    charset.remove(s[start])
-                    start += 1
-                if s[start] == char:
-                    charset.remove(s[start])
-                    start += 1
-                charset.add(char)
-        maxLength = max(maxLength, len(charset))
+        charIndexMap = {}
+        
+        for i, char in enumerate(s):
+            if char in charIndexMap and charIndexMap[char] >= start:
+                start = charIndexMap[char] + 1
+            charIndexMap[char] = i
+            maxLength = max(maxLength, i - start + 1)
+        
         return maxLength
