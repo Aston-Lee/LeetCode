2ee2746bd4ee05ab55dd2990a63a92adc7b9b4ed
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-10-11 13:38:12 -0500

    Time: 1527 ms (34.97%), Space: 20.5 MB (53.85%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,28 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
-        output = []
         nums.sort()
-        
-        for i in range(len(nums)-2):
-            if i>0 and nums[i] == nums[i-1]:
-                continue
-            l, r = i+1, len(nums)-1
+        res = []
+        t = 0
+        while t != len(nums):
+            
+            l, r = t+1, len(nums)-1
             while l < r:
-                curr_sum = nums[i] + nums[l] + nums[r]
-                if curr_sum == 0:
-                    output.append([nums[i], nums[l], nums[r]])
-                    while l < r and nums[l] == nums[l+1]:
+                if nums[t] + nums[l] + nums[r] == 0:
+                    res.append([nums[t],nums[l],nums[r]])
+                    while l+1 != len(nums) and nums[l] == nums[l+1]:
                         l += 1
-                    while l < r and nums[r] == nums[r-1]:
+                    while r-1 != 0 and nums[r] == nums[r-1]:
                         r -= 1
                     l += 1
                     r -= 1
-                elif curr_sum < 0:
+                elif nums[t] + nums[l] + nums[r] < 0:
                     l += 1
                 else:
                     r -= 1
-        return output
+            
+            while t != len(nums)-1 and nums[t] == nums[t+1]:
+                t += 1
+            t += 1
+            
+        return res
