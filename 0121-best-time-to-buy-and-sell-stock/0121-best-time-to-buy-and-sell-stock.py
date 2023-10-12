4b88613574cc6749ee58b635091b4cabad34521c
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-10-12 13:06:41 -0500

    Time: 758 ms (69.59%), Space: 22.4 MB (85.62%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,50 +4,20 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        # @leftmost rightmost -> is used for rain, not day trade
         
-#         # two pointer?
-#         least = 1000000  # by not using max() its so much faster 
-#         value = 0
-#         maxvalue =0
-        
-#         for i in prices:
-#             if least > i:
-#                 least = i
-#                 value = 0
-#             else:
-#                 value = i - least
-#                 if maxvalue < value:
-#                     maxvalue = value
-                    
-#         return maxvalue
-
-        # # @2 pointer Time:O(n) Space:O(1)
-        # l, r = 0, 1
-        # maxP = 0
-        # while r<len(prices):
-        #     if prices[l] < prices[r]:
-        #         maxP = max( maxP, prices[r] - prices[l] )
-        #     else:
-        #         l = r
-        #     r += 1 
-        # return maxP
-        
-        currmin, currmax = float('inf'), -float('inf')
+        currmin, currmax = prices[0], prices[0]
         maxprofit = 0
         for p in prices:
-            if p < currmin and currmin != float('inf'):
-                if currmax != -float('inf'):
-                    maxprofit = max(maxprofit, currmax - currmin)
-                currmin = p
-                currmax = -float('inf')
+            if p < currmin:
+                maxprofit = max(maxprofit, currmax-currmin)
+                currmin, currmax = p, p
             else:
-                currmin = min(currmin, p)
                 currmax = max(currmax, p)
-        
-        maxprofit = max(maxprofit, currmax - currmin)
+        maxprofit = max(maxprofit, currmax-currmin)
         return maxprofit
-            
+        
+        
+        
             
             
             
