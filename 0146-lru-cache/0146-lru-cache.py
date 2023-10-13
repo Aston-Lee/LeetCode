a14a1510171eb638b8fcafebcf3f1f956525006f
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-10-12 22:23:32 -0500

    Time: 584 ms (92.71%), Space: 77.3 MB (88.85%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,60 +1,83 @@
-## Double Link List Version
-class Node:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.prev = None
-        self.next = None
+import collections
 
 class LRUCache:
-
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}
-        self.head = Node(0,0)
-        self.tail = Node(0,0)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        
+        self.dic = collections.OrderedDict()
+
     def get(self, key: int) -> int:
-        if key in self.cache:
-            node = self.cache[key]
-            self._move_to_head(node)
-            return node.value
-        else:
+        if key not in self.dic:
             return -1
-
+        
+        self.dic.move_to_end(key)
+        return self.dic[key]
+        
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            node = self.cache[key]
-            node.value = value
-            self._move_to_head(node)
-        else:
-            if len(self.cache) == self.capacity:
-                del self.cache[self.tail.prev.key]
-                self._remove(self.tail.prev)
-            node = Node(key, value)
-            self.cache[key] = node
-            self._add(node)
-            
-    def _move_to_head(self, node):
-        self._remove(node)
-        self._add(node)
-            
-    def _add(self, node):
-        node.prev = self.head
-        node.next = self.head.next
-        self.head.next.prev = node
-        self.head.next = node
+        if key in self.dic:
+            self.dic.move_to_end(key)
         
-    def _remove(self, node):
-        node.prev.next = node.next
-        node.next.prev = node.prev
+        self.dic[key] = value
+        if len(self.dic) > self.capacity:
+            self.dic.popitem(last=False)
+
+
+# ## Double Link List Version
+# class Node:
+#     def __init__(self, key, value):
+#         self.key = key
+#         self.value = value
+#         self.prev = None
+#         self.next = None
+
+# class LRUCache:
+
+#     def __init__(self, capacity: int):
+#         self.capacity = capacity
+#         self.cache = {}
+#         self.head = Node(0,0)
+#         self.tail = Node(0,0)
+#         self.head.next = self.tail
+#         self.tail.prev = self.head
+        
+#     def get(self, key: int) -> int:
+#         if key in self.cache:
+#             node = self.cache[key]
+#             self._move_to_head(node)
+#             return node.value
+#         else:
+#             return -1
+
+#     def put(self, key: int, value: int) -> None:
+#         if key in self.cache:
+#             node = self.cache[key]
+#             node.value = value
+#             self._move_to_head(node)
+#         else:
+#             if len(self.cache) == self.capacity:
+#                 del self.cache[self.tail.prev.key]
+#                 self._remove(self.tail.prev)
+#             node = Node(key, value)
+#             self.cache[key] = node
+#             self._add(node)
+            
+#     def _move_to_head(self, node):
+#         self._remove(node)
+#         self._add(node)
+            
+#     def _add(self, node):
+#         node.prev = self.head
+#         node.next = self.head.next
+#         self.head.next.prev = node
+#         self.head.next = node
+        
+#     def _remove(self, node):
+#         node.prev.next = node.next
+#         node.next.prev = node.prev
         
         
 
 
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+# # Your LRUCache object will be instantiated and called as such:
+# # obj = LRUCache(capacity)
+# # param_1 = obj.get(key)
+# # obj.put(key,value)
