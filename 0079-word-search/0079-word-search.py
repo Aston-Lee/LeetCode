6e306a248b9880a72ddad762e8e77e02b590a18e
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-10-16 13:37:17 -0500

    Time: 687 ms (96.73%), Space: 16.3 MB (82.18%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,51 +1,45 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        ## use copy.deepcopy for deepcopy
-        ## in order to fit the time complexity, check the less freq char in words and invert if you need to 
-        ## if boardDict[word[0]] > boardDict[word[-1]]:
-        ##    word = word[::-1]
         
-        neighbor = ((0,1), (1,0), (0,-1), (-1,0))
+        ROWS, COLS = len(board), len(board[0])
+        path = set()
         
-        m = len(board)
-        n = len(board[0])
-        
-        def dfs(i, j, remain, board):
-            # print(i, j, remain, board)
-            if remain == "":
-                return True
-            for path in neighbor:
-                if (0 <= i + path[0] and i + path[0] < m) \
-                    and (0 <= j + path[1] and j + path[1] < n) \
-                    and board[i + path[0]][j + path[1]] == remain[0]:
-                        tmp = board
-                        board[i][j] = -1
-                        if dfs(i + path[0], j + path[1], remain[1:], copy.deepcopy(board) ) :
-                            return True
-                        board = tmp
-            return False
-        
-        boardDict = {}
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] in boardDict:
-                    boardDict[board[i][j]] += 1
-                else:
-                    boardDict[board[i][j]] = 1
-        
-        wordDict = Counter(word)
-        for k in wordDict:
-            if k not in boardDict or wordDict[k] > boardDict[k]:
+        NEI = [(0,1), (1,0), (0,-1), (-1,0)]
+        def dfs(r, c, i):
+            if (r,c) in path:
                 return False
+            if i == len(word)+1:
+                return True
             
-        ## this is so crucial
-        if boardDict[word[0]] > boardDict[word[-1]]:
+            if r < 0 or r >= ROWS or c < 0 or c >= COLS:
+                return False
+            elif board[r][c] != word[i-1]:
+                return False
+                
+            path.add((r,c))
+            for x, y in NEI:
+                if dfs(r+x,c+y,i+1):
+                    return True
+            path.remove((r,c))
+            
+            return False
+                
+            
+            
+        freq = collections.Counter(word)
+        if freq[word[0]] > freq[word[-1]]:
             word = word[::-1]
-        
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                if board[i][j] == word[0]:
-                    if dfs(i, j, word[1:], copy.deepcopy(board)) == True:
-                        return True
-        
+             
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == word[0]:
+                    if dfs(r,c,1):
+                        return True        
         return False
+        
+        
+            
+            
+            
+            
+        
