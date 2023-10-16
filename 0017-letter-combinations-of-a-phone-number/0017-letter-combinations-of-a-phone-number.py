91fe3b084b486c9cc45665069ab45354af89abff
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-10-16 14:10:23 -0500

    Time: 39 ms (49.73%), Space: 16.4 MB (35.54%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,7 +1,6 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         
-
         
         charmap = {
             "2" : "abc",
@@ -14,56 +13,23 @@ class Solution:
             "9" : "wxyz"  
         }
         
-        Res = []
-        if digits == "":
-            return Res
-        def backtrack(tmpRes, remaindigits):
-            if remaindigits == "":
-                Res.append(tmpRes)
+        res = []
+        strings = []
+        for dig in digits:
+            strings.append(charmap[dig])
+            
+        def bt(pos, currstr):
+            if len(currstr) == len(digits):
+                res.append(currstr)
                 return 
-            backup = tmpRes
-            for char in charmap[remaindigits[0]]:
-                tmpRes += char
-                backtrack(tmpRes, remaindigits[1:])                
-                tmpRes = backup
-        backtrack("", digits)
-        return Res
+            for i, c in enumerate(strings[pos]):
+                bt(pos+1, currstr+c)
+            return 
+            
+        if digits == "":
+            return []
         
-        
-        
-        
-        
-        
-        
-        
-#         charmap = {
-#             "2" : "abc",
-#             "3" : "def",
-#             "4" : "ghi",
-#             "5" : "jkl",
-#             "6" : "mno",
-#             "7" : "pqrs",
-#             "8" : "tuv",
-#             "9" : "wxyz"  
-#         }
-
-#         res = []
-#         n = len(digits)
-#         def backtrack(prev, remain):
-#             tmp_prev = prev
-#             for char in charmap[remain[0]]:
-#                 prev += char
-#                 if len(prev) == n:
-#                     res.append(prev)
-#                     prev = tmp_prev
-#                 else:
-#                     backtrack(prev, remain[1:] )
-#                     prev = tmp_prev
-#             return
-
-#         if not digits:
-#             return []
-
-#         backtrack("", digits)
-#         return res
+        bt(0, "")
+        return res
+    
         
