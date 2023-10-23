0308a7e38df3e6832529d1c452335154d08f9f7f
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-10-22 21:36:13 -0500

    Time: 33 ms (89.56%), Space: 16.2 MB (70.44%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.py
+++ 0342-power-of-four/0342-power-of-four.py
@@ -2,8 +2,11 @@ class Solution:
     def isPowerOfFour(self, n: int) -> bool:
         if n == 1: return True
         if n == 0: return False
+        if n < n:
+            n = -n
         binary = bin(n)[2:]
         length = len(binary)
+        print(binary, length)
         return int(binary[1:]) == 0 and (length-1)%2 == 0
         
         
