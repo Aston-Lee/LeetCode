c5d29fe69fcf7ab94d3eba07bbe422cb7729ea4f
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-10-27 12:42:48 -0500

    Time: 588 ms (48.67%), Space: 16.4 MB (54.45%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,35 +1,40 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         
-        # center, diameter
-        ## check the updating logic
         
-        if len(s) == 1:
-            return s
-        
-        res = s[0]
-        
-        for i in range(len(s)):
-            diameter = 0
-            while i - diameter != -1 and i + diameter != len(s):
-                if s[i - diameter] == s[i + diameter]:
-                    diameter += 1
-                else:
+        # babad
+        # lcr
+        # c = 1
+        # d = 0
+        ## single
+        res = [-float('inf'), [None, None]]
+        for c in range(len(s)):
+            d = 0
+            l, r = c, c
+            while True:
+                l, r = c-d, c+d
+                if not (0 <= l) or not (r < len(s)) or (s[l] != s[r]):
                     break
-            if 2*diameter - 1 > len(res):
-                res = s[i - diameter + 1 : i + diameter]
-            
-        for i in range(len(s)-1):
-            diameter = 0
-            if s[i] == s[i+1]:
-                while i - diameter != -1 and i+1 + diameter != len(s):
-                    if s[i - diameter] == s[i+1 + diameter]:
-                        diameter += 1
-                    else:
-                        break
-                if 2*diameter > len(res):
-                    res = s[i - diameter + 1 : i+1 + diameter]
-                    
-        return res
+                if (r-l+1 > res[0]):
+                    res = [r-l+1, [l, r]]
+                d += 1
+    
+        ## double
+        for c in range(len(s)-1):
+            d = 0
+            l, r = c, c+1
+            while True:
+                l, r = c-d, c+1+d
+                # print(l, r, d)
+                if not (0 <= l) or not (r < len(s)) or (s[l] != s[r]):
+                    break
+                if (r-l+1 > res[0]):
+                    res = [r-l+1, [l, r]]
+                d += 1
+                
+        return s[res[1][0]:res[1][1]+1]
         
+        
+        
+          
         
