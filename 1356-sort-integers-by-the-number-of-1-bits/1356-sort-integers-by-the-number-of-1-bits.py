7fa26b84cde261df0cb9d5289713ce933cf331fa
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-10-30 12:43:49 -0500

    Time: 91 ms (10.55%), Space: 16.3 MB (94.41%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,50 +1,18 @@
 class Solution:
     def sortByBits(self, arr: List[int]) -> List[int]:
         
-        onemap = defaultdict(int)
-        
-        def getone(n):
-            cnt = 0 
-            while n:
-                if n & 1 :
-                    cnt+=1
-                n>>=1
-            return cnt 
-        
-        for n in arr:
-            onemap[n]= getone(n)
-        
-        def mergesort(array):
-            if len(array) <= 1:
-                return array
-            mid = len(array) // 2
-            left = mergesort(array[:mid])
-            right = mergesort(array[mid:])
-            return merge(left, right)
-        
-        def merge(arr1, arr2):
-            l, r = 0, 0
-            tmparr = []
-            while l<len(arr1) and r<len(arr2):
-                if onemap[arr1[l]] < onemap[arr2[r]]:
-                    tmparr.append(arr1[l])
-                    l += 1
-                elif onemap[arr1[l]] > onemap[arr2[r]]:
-                    tmparr.append(arr2[r])
-                    r += 1
-                else:
-                    if arr1[l] < arr2[r]:
-                        tmparr.append(arr1[l])
-                        l += 1
-                    else:
-                        tmparr.append(arr2[r])
-                        r += 1
-                    
-            tmparr += arr1[l:] + arr2[r:]
-            return tmparr  
+        def getbits(num) -> int: # return how many bits for this num
+            return sum( int(bit) for bit in bin(num)[2:])
+          
+        minheap = []
+        for num in arr:
+            heappush(minheap,  (getbits(num), num))
             
-        return mergesort(arr) 
+        result = []
+        while minheap:
+            result.append(heappop(minheap)[1])
             
+        return result
             
             
         
