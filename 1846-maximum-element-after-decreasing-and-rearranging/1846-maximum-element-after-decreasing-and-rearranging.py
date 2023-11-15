e50c329c7aada86a558b1d0253cddfb2dd3bd663
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-11-14 20:40:58 -0600

    Time: 515 ms (5.52%), Space: 27.1 MB (6.08%) - LeetHub

---
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumElementAfterDecrementingAndRearranging(self, arr: List[int]) -> int:
+        
+        
+        minheap = sorted(arr)
+        arr[0] = 1
+        heapq.heapify(minheap)
+        
+        heapq.heappop(minheap)
+        largest = 1
+        while minheap:
+            if abs(minheap[0]-largest) <= 1:
+                if minheap[0] > largest:
+                    largest = minheap[0]
+            else:
+                largest += 1
+            heapq.heappop(minheap)
+                
+        return largest
+            
+        
+        
