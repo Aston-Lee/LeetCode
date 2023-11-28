ed3695d9a93fd6034d5c4b49f19303119b23c0ed
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-11-27 20:10:28 -0600

    Time: 117 ms (5.10%), Space: 24.5 MB (5.80%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,45 +1,57 @@
+import numpy as np
+
 class Solution(object):
     def isValidSudoku(self, board):
         """
         :type board: List[List[str]]
         :rtype: bool
         """
-        N = len(board)
-        # check box
-        for i in range(N/3):
-            for j in range(N/3):
-                seen = set()
-                for k in range(3):
-                    for z in range(3):
-                        if board[3*i+k][3*j+z] != '.': 
-                            if board[3*i+k][3*j+z] in seen:
-                                return False
-                            else:
-                                seen.add(board[3*i+k][3*j+z])
-                      
-        # check row
-        for i in range(N):
-            seen = set()
-            for j in range(N):
-                if board[i][j] != '.':
-                    if board[i][j] in seen:
-                        return False
-                    else:
-                        seen.add(board[i][j])
         
-        #check column
-        for j in range(N):
-            seen = set()
-            for i in range(N):
-                if board[i][j] != '.':
-                    if board[i][j] in seen:
+        def check(idx):
+            appeared = set()
+            for num in board[idx]:
+                if num == '.':
+                    continue
+                if num in appeared:
+                    return False
+                appeared.add(num)
+            return True
+                
+            
+        def checkblock(i, j):
+            appeared = set()
+            for _x in range(3):
+                for _y in range(3):
+                    if board[i+_x][j+_y] == '.':
+                        continue
+                    if board[i+_x][j+_y] in appeared:
                         return False
-                    else:
-                        seen.add(board[i][j])
+                    appeared.add(board[i+_x][j+_y])
+            return True
         
+            
+        for i in range(9):
+            if not check(i):
+                return False
+        
+            
+        for i in range(0,9,3):
+            for j in range(0,9,3):
+                if not checkblock(i,j):
+                    return False
+           
+        
+        board = np.transpose(board)
+        for i in range(9):
+            if not check(i):
+                return False   
+            
         return True
         
-        # check row
+            
+            
+            
+        
         
         
         
