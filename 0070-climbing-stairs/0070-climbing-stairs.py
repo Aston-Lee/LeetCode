7d93a3b7c6b7d51adf85ff704fb03d6c3bc345f4
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-11-29 19:18:03 -0600

    Time: 40 ms (42.06%), Space: 16.4 MB (10.01%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,11 +1,16 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         
-        @cache
+        # @cache
+        mem = {}
         def dp(i):
             if i == 0 or i == 1:
                 return 1
-            return dp(i-1) + dp(i-2)
+            if i in mem:
+                return mem[i]
+            ans = dp(i-1) + dp(i-2)
+            mem[i] = ans
+            return ans
         
         return dp(n)
         
