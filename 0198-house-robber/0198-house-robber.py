7a6a41fcc962dbd1d15c573a5490feb4efc74838
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-11-30 19:23:36 -0600

    Time: 33 ms (90.94%), Space: 16.4 MB (18.64%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,15 +1,26 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        dp = [0]*(len(nums)+1)
+        # [2,7,9,3,1]
+        # [2,7,11,10,12]
         
-        for i in range(len(nums)+1):
-            if i<3:
-                dp[i] = nums[i-1]
-            elif i == 3:
-                dp[i] = max(dp[1] + nums[2], dp[2])
+        n = len(nums)   
+        if n < 3:
+            return max(nums)
+        elif n == 3:
+            nums[2] += nums[0] 
+            return max(nums)
+        
+        dp = [0]*n
+        
+        for i in range(n):
+            if i == 0 or i == 1:
+                dp[i] = nums[i]
+                
+            elif i == 2:
+                dp[i] = dp[0] + nums[2]
+            
             else:
-                dp[i] = max(dp[i-2], dp[i-3]) + nums[i-1]
-            print(dp)
-        
-        return max(dp[-1], dp[-2])
+                dp[i] = max(dp[i-2], dp[i-3]) + nums[i]
+                
+        return max(dp)
