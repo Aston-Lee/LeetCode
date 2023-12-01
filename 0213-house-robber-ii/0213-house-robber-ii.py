e46ec35f1e0668dc94b321a80ade55f902803cd9
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-11-30 19:36:45 -0600

    Time: 46 ms (15.13%), Space: 16.3 MB (20.82%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -7,17 +7,23 @@ class Solution:
             return nums[0]
         if len(nums) == 2:
             return max(nums[0], nums[1])
+        
         dp1 = [0] * (len(nums))
         dp1[0] = nums[0]
         dp1[1] = max(nums[0], nums[1])
         for i in range(2, len(nums)):
             dp1[i] = max(dp1[i-1], dp1[i-2] + nums[i])
+            
         dp2 = [0] * (len(nums))
         dp2[0] = 0
         dp2[1] = nums[1]
         for i in range(2, len(nums)):
             dp2[i] = max(dp2[i-1], dp2[i-2] + nums[i])
         return max(dp1[-2], dp2[-1])
+    
+    
+solution = Solution()
+print(solution.rob([2,3,2]))
             
             
             
