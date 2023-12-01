2e01c95e791ac2aecb68f9e355a447e9fd0f8814
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-11-30 19:52:33 -0600

    Time: 34 ms (88.56%), Space: 16.4 MB (5.53%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -1,29 +1,36 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        if len(nums) == 0:
-            return 0
+#         [200,3,140,20,10]
+        
+#         [200,3,340,220] 340
+#         [3,140,23,150] 150
+        
+        
         if len(nums) == 1:
             return nums[0]
-        if len(nums) == 2:
-            return max(nums[0], nums[1])
         
-        dp1 = [0] * (len(nums))
-        dp1[0] = nums[0]
-        dp1[1] = max(nums[0], nums[1])
-        for i in range(2, len(nums)):
-            dp1[i] = max(dp1[i-1], dp1[i-2] + nums[i])
-            
-        dp2 = [0] * (len(nums))
-        dp2[0] = 0
-        dp2[1] = nums[1]
-        for i in range(2, len(nums)):
-            dp2[i] = max(dp2[i-1], dp2[i-2] + nums[i])
-        return max(dp1[-2], dp2[-1])
-    
-    
-solution = Solution()
-print(solution.rob([2,3,2]))
-            
-            
+        ls1 = nums[:-1]
+        ls2 = nums[1:]
+        
+        def robber(ls):
+            n = len(ls)
+            if n<2:
+                return max(ls)
+            if n == 3:
+                ls[2] += ls[0]
+                return max(ls)
+            dp = [0]*n
+            for i in range(n):
+                if i<2:
+                    dp[i] = ls[i]
+                elif i==2:
+                    dp[2] = dp[i-2] + ls[i]
+                else:
+                    dp[i] = max(dp[i-2], dp[i-3]) + ls[i]
+            print(dp)
+            return max(dp)
+                    
+        return max(robber(ls1), robber(ls2))
+                
             
