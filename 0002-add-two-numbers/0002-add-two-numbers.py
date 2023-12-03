91defa45f46a8766d765d88a76348ab12c396296
Aston <76436175+Aston-Lee@users.noreply.github.com>
2023-12-03 12:00:36 -0600

    Time: 60 ms (81.86%), Space: 16.3 MB (41.84%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,31 +5,55 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        tmp = 0
-        l3 = pointer = ListNode(0)
-        while(l1 or l2):
-            if l1: n1 = l1.val
-            else: n1 = 0
-            if l2: n2 = l2.val
-            else: n2 = 0    
+        
+        ptr = node = ListNode(0)
+        num = 0
+        inc = 0
+        while l1 and l2:
+            num = (l1.val + l2.val + inc)
+            if num >= 10:
+                num %= 10
+                inc = 1
+            else:
+                inc = 0
+            node.next = ListNode(num)
+            l1 = l1.next
+            l2 = l2.next
+            node = node.next
             
-            total = n1 + n2 + tmp
-            tmp = 0
-            if total >= 10:
-                tmp = 1
-                total %= 10
-            l3.next = ListNode(total)
-            l3 = l3.next
-            if l1:
+        ## one not ended
+        if l1 and not l2:
+            while l1:
+                num = (l1.val + inc)
+                if num >= 10:
+                    num %= 10
+                    inc = 1
+                else:
+                    inc = 0
+                node.next = ListNode(num)
                 l1 = l1.next
-            if l2:
+                node = node.next
+            node.next = None
+            
+        elif not l1 and l2:
+            while l2:
+                num = (l2.val + inc)
+                if num >= 10:
+                    num %= 10
+                    inc = 1
+                else:
+                    inc = 0
+                node.next = ListNode(num)
                 l2 = l2.next
+                node = node.next
+            node.next = None
+            
+        if not l1 and not l2:
+            if inc == 1:
+                node.next = ListNode(1)
+                node = node.next
+            node.next = None
+        else:
+            print('how')
         
-        if tmp == 1:
-            l3.next = ListNode(1)
-            l3.next.next = None
-        
-        return pointer.next
-            
-            
-            
+        return ptr.next
