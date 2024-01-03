28fe177739641fd8c2f8645bcaa6f50e36f6adc7
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-01-03 00:57:07 -0600

    Time: 37 ms (87.32%), Space: 17.3 MB (15.75%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,13 +1,73 @@
 class Solution:
     def predictTheWinner(self, nums: List[int]) -> bool:
+        '''
+        return True representing player1 can win
+        
+        [1,5,233,7,2]
+        
+        if 1 - best([5,233,7,2]) -> -234
+               return max( 5 - best(233,7,2), 7 - best(5,233,7) )
+                      max( 5 - 233,           7 - -228) -> 235
+                               return max((233 - best(7,2)), (2 - best(233,7))
+                                                  return max((5-best(233,7)), (7-best(5,233)))
+        if 2 - best([1,5,233,7]) -> 
+               return max( 1 - best(5, 233,7), 7 - best(1,5,233) )
+               return max( 1- -228,            7 - 228) -> 229
+                               return max((5 - best(233,7)), (7 - best(5, 233))
+                                                   return max((1 - best(5,233)), (233 - best(1,5))
+                
+        [1,5,2]        
+        if 1 - best([5,2]) > 0: return True -> 1-5 = -4 < 0 no return 
+               max(5,2) -> 5
+                
+        if 2 - best([1,5]) > 0: return True -> 2-5 = -3 < 0 no return 
+               max(1,5) -> 5
+        
+        return False
+
+
+        [1,5,2,4,6]
+        1 - best([5,2,4,6]) -> 1 - 5 = -4
+            5 - best([2,4,6]) -> 5-2 = 3
+                2 - best([4,6]) -> -4
+                6 - best([2,4]) -> 2
+            6 - best([5,2,4]) -> 6-1 = 5
+                5 - best([2,4]) -> 1
+                4 - best([5,2]) -> -1
+                
+        
+        '''
+        
+#         if len(nums) <= 2:
+#             return True
+        
+#         def best(array):
+#             if len(array) == 2:
+#                 return max(array)
+#             else:   
+#                 return max(array[0] - best(array[1:]), array[-1] - best(array[:-1]))
+
+#         if nums[0] - best(nums[1:]) >= 0:
+#             return True
+        
+#         if nums[-1] - best(nums[:-1]) >= 0:
+#             return True
+        
+#         return False
+
         n = len(nums)
+        if n % 2 == 0 or n == 1:
+            return True
         
-        def maxDiff(left, right):
-            if left == right:
-                return nums[left]
-            score_by_left = nums[left] - maxDiff(left + 1, right)
-            score_by_right = nums[right] - maxDiff(left, right - 1)
-            # print(left, right, score_by_left, score_by_right)
-            return max(score_by_left, score_by_right)
+        dp = [[0] * n for _ in range(n)]
+
+        for i in range(n):
+            dp[i][i] = nums[i]
+
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                dp[i][j] = max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1])
+
+        return dp[0][n - 1] >= 0
         
-        return maxDiff(0, n - 1) >= 0
