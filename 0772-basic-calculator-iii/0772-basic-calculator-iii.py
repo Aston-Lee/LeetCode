6ecf6dcc08b61aaf03b669a392792da768baa492
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-01-04 06:33:46 -0600

    Time: 42 ms (76.38%), Space: 17.3 MB (20.93%) - LeetHub

--- 0772-basic-calculator-iii/0772-basic-calculator-iii.py
+++ 0772-basic-calculator-iii/0772-basic-calculator-iii.py
@@ -1,61 +1,58 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        # Function to apply the operation based on the last sign
-        def apply_operation(left, right, op):
-            if op == '+':
-                return left + right
-            elif op == '-':
-                return left - right
-            elif op == '*':
-                return left * right
-            elif op == '/':
-                return int(left / right)  # Integer division that truncates towards zero
-
-        # Stack to handle parentheses and operators
+        '''
+        "2*(5+5*2)/3+(6/2+8)@"
+        prevsign +
+        num = 11
+        stack = [10,]
+        '''
+        
+        def _calculate(num, sign):
+            if sign == '+':
+                return num
+            elif sign == '-':
+                return -num
+            elif sign == '*':
+                n = stack.pop()
+                return int(n*num)
+            elif sign == '/':
+                n = stack.pop()
+                return int(n/num)
+            else:
+                print("how?")
+            
+            
+        num = 0
+        prevsign = '+'
         stack = []
-        current_num = 0
-        last_operation = '+'
-
-        for char in s + '+':
+        s += '@'
+        
+        for char in s:
+            
             if char.isdigit():
-                # Construct the current number
-                current_num = current_num * 10 + int(char)
-            elif char in '+-*/':
-                # Apply the last operation
-                if stack and stack[-1] in '*/':
-                    # Apply the last high precedence operation
-                    last_high_precedence_operation = stack.pop()
-                    last_num = stack.pop()
-                    current_num = apply_operation(last_num, current_num, last_high_precedence_operation)
-
-                if char in '+-' or char == '+':
-                    # Apply the last low precedence operation and reset current_num
-                    while stack and stack[-1] in '+-':
-                        last_low_precedence_operation = stack.pop()
-                        last_num = stack.pop()
-                        current_num = apply_operation(last_num, current_num, last_low_precedence_operation)
-
-                # Update the operation and push to stack
-                last_operation = char
-                stack.append(current_num)
-                stack.append(last_operation)
-                current_num = 0
-
+                num = num*10 + int(char)
+                
+            elif char in "+-*/":
+                stack.append(_calculate(num, prevsign))
+                num = 0
+                prevsign = char
+                
             elif char == '(':
-                # Push the last operation and reset for new context
+                stack.append(prevsign)
                 stack.append('(')
+                num = 0
+                prevsign = '+'
+                
             elif char == ')':
-                # Resolve the current context
-                while stack and stack[-1] != '(':
-                    last_operation = stack.pop()
-                    last_num = stack.pop()
-                    current_num = apply_operation(last_num, current_num, last_operation)
-                stack.pop()  # Pop the '('
-
-        # Final operation, if any
-        while stack:
-            last_operation = stack.pop()
-            last_num = stack.pop()
-            current_num = apply_operation(last_num, current_num, last_operation)
-
-        return current_num
+                stack.append(_calculate(num, prevsign))
+                num = 0
+                # prevsign = '+'
+                while stack[-1] != '(':
+                    num += stack.pop()
+                stack.pop()
+                prevsign = stack.pop()
+                
+            else: #'@'
+                stack.append(_calculate(num, prevsign))
+                
+        return sum(stack)
