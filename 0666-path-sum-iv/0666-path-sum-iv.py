1b213d76ff97fb1a64509045b0d85bbf043abd98
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-01-04 02:26:53 -0600

    Time: 33 ms (91.54%), Space: 17.6 MB (7.41%) - LeetHub

--- 0666-path-sum-iv/0666-path-sum-iv.py
+++ 0666-path-sum-iv/0666-path-sum-iv.py
@@ -1,32 +1,40 @@
-class Solution(object):
-    def pathSum(self, nums):
+class Solution:
+    def pathSum(self, nums: List[int]) -> int:
         
-        self.ans = 0
-        PosVal = {}
+        '''
+        
+        [113,215,221,312,322,332,345]
+        11 -> 21, 22
+        2*(n-1)+1, 2*(n-1)+2
+        
+        dict { 
+            11: 3
+            21: 5
+            22: 1
+        }
+        '''
+        
+        LocNum = {}
         for num in nums:
-            PosVal[num//10] = num%10
-            
-        def dfs(pos, pathsum):
-            if pos not in PosVal:
+            LocNum[num//10] = num%10
+          
+        self.ans = 0
+        def dfs(node, tmp):
+            tmp += LocNum[node]
+            left = ((node//10)+1)*10 + 2*(node%10-1) + 1
+            right = ((node//10)+1)*10 + 2*(node%10-1) + 2
+            if left not in LocNum and right not in LocNum:
+                self.ans += tmp 
                 return 
-            pathsum += PosVal[pos]
-            
-            depth, position = divmod(pos, 10)
-            left = (depth+1)*10 + 2*position-1 ## note this
-            right = left + 1
-            
-            if left not in PosVal and right not in PosVal:
-                self.ans += pathsum
-            dfs(left, pathsum)
-            dfs(right, pathsum)
-                
-            
-        dfs(nums[0]//10, 0)
-        return self.ans
-            
-            
-            
+            if left in LocNum:
+                dfs(left, tmp)
+            if right in LocNum:
+                dfs(right, tmp)
+            return 
         
+        dfs(11, 0)
         
-        
+        return self.ans 
+            
+            
         
