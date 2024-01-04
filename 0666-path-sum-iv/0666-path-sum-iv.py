a68988030f8db1ee212aa749a909d72e89fbe473
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-01-04 02:11:42 -0600

    Time: 42 ms (45.45%), Space: 17.4 MB (7.41%) - LeetHub

--- 0666-path-sum-iv/0666-path-sum-iv.py
+++ 0666-path-sum-iv/0666-path-sum-iv.py
@@ -10,16 +10,13 @@ class Solution(object):
             if pos not in PosVal:
                 return 
             pathsum += PosVal[pos]
+            
             depth, position = divmod(pos, 10)
             left = (depth+1)*10 + 2*position-1 ## note this
             right = left + 1
+            
             if left not in PosVal and right not in PosVal:
                 self.ans += pathsum
-#             if left in PosVal:
-#                 dfs(left, pathsum)
-#             if right in PosVal:
-#                 dfs(right, pathsum)
-                
             dfs(left, pathsum)
             dfs(right, pathsum)
                 
