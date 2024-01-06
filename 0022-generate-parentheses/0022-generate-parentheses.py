932b9c03140673a48e0484a1c1d37de55f37ce5f
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-01-05 21:38:46 -0600

    Time: 50 ms (19.14%), Space: 17.7 MB (16.05%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,31 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        '''
+        n = 3
+        
+        () n=2
+
+        (()) n=1,                  ()() n=1
+        
+        ((())) n=0, (()()) n=0     (())() n=0,  ()(()) n=0  ()()() n=0
+        
+        '''
+        
+        ans = set()
+        def addPar(shape, tn):
+            if tn == 0:
+                ans.add(shape)
+                return
+            for i, s in enumerate(shape):
+                if s == '(':
+                    tmp = shape
+                    addPar(shape[:i+1]+'()'+shape[i+1:], tn-1)
+                    
+            addPar(shape+'()', tn-1)
+            return 
+                    
+                    
+        addPar("", n)
+        return list(ans)
+                    
+            
