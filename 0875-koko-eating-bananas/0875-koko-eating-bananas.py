ed9f5e96a5740ec311e0715fd77d5c04da8aa17b
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-01-09 06:47:40 -0600

    Time: 3258 ms (5.01%), Space: 19 MB (10.43%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,21 +1,32 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        if h == len(piles):
-            return max(piles)
+        '''
+        piles = [3,6,7,11], h = 8
+        minK = 1, maxK = max(piles)
+        bs(l=1, r=11)
         
-        l, r = 1 ,max(piles)
-        minK = float('inf')
-        while l<=r:
-            amount = (l+r)//2
-            currh = 0
-            # for p in piles:
-            #     currh += math.ceil(p/amount)
-            currh = sum([math.ceil(p/amount) for p in piles])
-            
-            if currh > h:
-                l = amount+1
-            else:
-                minK = min(minK, amount)
-                r = amount-1
+        '''
+        l, r = 1, max(piles)
+        while l <= r:
+            # print(l, r)
+            mid = (l+r)//2
+            time = 0
+            for p in piles:
+                time += math.ceil(p/mid)
+            if time == h:
+                k = mid
+                tmp = h
+                while tmp == h:
+                    k -= 1
+                    if k == 0:
+                        return 1
+                    tmp = 0
+                    for p in piles:
+                        tmp += math.ceil(p/k)
+                return k + 1
+            elif time > h: # k too small
+                l = mid + 1
+            else: # time < h
+                r = mid - 1
                 
-        return minK
+        return l
