1916f846413a1dd9b90e2f3fdc16c692aa70d509
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-01-17 06:00:36 -0600

    Time: 341 ms (79.45%), Space: 17.3 MB (45.44%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,53 +1,37 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         
-        ## try 2 pointer 
-        ## "babad"
+        
         longest = -float('inf')
+        ans = []
+        #check single 
         for p in range(len(s)):
-            d = 0
-            while 0 <= p-d and p+d < len(s):
-                if s[p-d] != s[p+d]:
+            l, r = p, p
+            while 0 <= l and r < len(s):
+                
+                if s[l] == s[r]:
+                    
+                    if longest < r-l+1:
+                        longest = r-l+1
+                        ans = [l,r]
+                else:
                     break
-                d += 1
-            d -= 1
-            if 2*d+1 > longest:
-                longest = 2*d+1
-                ans = [p-d, p+d]
-
-        ## "babbad", "cbbd"
+                l-=1
+                r+=1
+                
+        #check double 
         for p in range(len(s)-1):
-            p1, p2 = p, p+1
-            d = 0
-            while 0 <= p1-d and p2+d < len(s):
-                if s[p1-d] != s[p2+d]:
+            l, r = p, p+1
+            # print('---')
+            while 0 <= l and r < len(s):
+                if s[l] == s[r]:
+                    # print(l, r, s[l], s[r])
+                    if longest < r-l+1:
+                        longest = r-l+1
+                        ans = [l,r]
+                else:
                     break
-                d += 1
-            d -= 1
-            if 2*d+2 > longest:
-                longest = 2*d+2
-                ans = [p1-d, p2+d]
-        
-        return s[ans[0]: ans[1]+1]
-        
-#         n = len(s)
-#         dp = [[False] * n for _ in range(n)]
-#         ans = [0, 0]
-        
-#         for i in range(n):
-#             dp[i][i] = True
-        
-#         for i in range(n - 1):
-#             if s[i] == s[i + 1]:
-#                 dp[i][i + 1] = True
-#                 ans = [i, i + 1]
-
-#         for diff in range(2, n):
-#             for i in range(n - diff):
-#                 j = i + diff
-#                 if s[i] == s[j] and dp[i + 1][j - 1]:
-#                     dp[i][j] = True
-#                     ans = [i, j]
-
-#         i, j = ans
-#         return s[i:j + 1]
+                l-=1
+                r+=1
+                
+        return s[ans[0]:ans[1]+1]
