d6abbda7df06d52a67db162b9ea7f3de5da3b08d
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-01-28 02:46:59 -0600

    Time: 137 ms (70.37%), Space: 18.1 MB (58.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,4 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        return nums[len(nums)//2]
+        # nums.sort()
+        # return nums[len(nums)//2]
+        
+        maxVal = 0
+        ans = None
+        freq = collections.Counter(nums)
+        for key, val in freq.items():
+            if val > maxVal:
+                maxVal = val
+                ans = key
+            
+        return ans
