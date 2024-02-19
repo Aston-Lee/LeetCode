3d4e0f70fa56e676000473561e0641551b53cf1b
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-02-18 22:49:54 -0600

    Time: 228 ms (86.89%), Space: 18.9 MB (90.95%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,25 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
         
-        neighbor = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        DIR = ((0,1), (0,-1), (1,0), (-1,0))
         
-        def dfs(i, j):
-            # print(i, j)
+        m, n = len(grid), len(grid[0])
+        
+        def dfs(i,j):
             grid[i][j] = "0"
-            for a, b in neighbor:
-                if 0 <= i+a < m and 0 <= j+b < n and grid[i+a][j+b] == "1":
-                    dfs(i+a, j+b)
-            return
+            for _x, _y in DIR:
+                ni, nj = i+_x, j+_y
+                if 0<=ni<m and 0<=nj<n:
+                    if grid[ni][nj] == "1":
+                        dfs(ni,nj)
         
-        island = 0
+        ans = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
-                    island += 1
-                    dfs(i, j)
+                    ans += 1
+                    dfs(i,j)
+        
+        return ans
                     
-                    
-        return island
-                    
-            
+        
