7dbbf6a8f2d74a6542ced3dcfe5e8895331da459
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-02-18 20:58:21 -0600

    Time: 119 ms (76.42%), Space: 30.8 MB (72.63%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,43 +1,40 @@
 class TrieNode:
-    def __init__(self, char):
-        self.isEnd = False
-        self.childs = {}
-        # self.char = ""
-        
-        
+    
+    def __init__(self):
+        self.child = {}
+        self.isEnd = False 
+
 class Trie:
 
     def __init__(self):
-        self.root = TrieNode("")
+        self.root = TrieNode()
 
     def insert(self, word: str) -> None:
         node = self.root
         for char in word:
-            if char not in node.childs:
-                node.childs[char] = TrieNode(char)
-            node = node.childs[char]
+            if char not in node.child:
+                node.child[char] = TrieNode()
+            node = node.child[char]
         node.isEnd = True
+        
 
     def search(self, word: str) -> bool:
         node = self.root
         for char in word:
-            if char not in node.childs:
+            if char not in node.child:
                 return False
-            else:
-                node = node.childs[char]
+            node = node.child[char]
         return node.isEnd
+            
         
-
     def startsWith(self, prefix: str) -> bool:
         node = self.root
         for char in prefix:
-            if char not in node.childs:
+            if char not in node.child:
                 return False
-            else:
-                node = node.childs[char]
+            node = node.child[char]
         return True
-        
-
+    
 
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
