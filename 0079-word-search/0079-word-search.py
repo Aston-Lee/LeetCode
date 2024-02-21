6c4e1885622889dd5ecb2d9ecf33bccf35fcc215
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-02-21 04:17:48 -0600

    Time: 3798 ms (71.45%), Space: 16.5 MB (84.78%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -5,6 +5,7 @@ class Solution:
         self.ans = False
         visited = set()
         def dfs(i, j, wordIndex):
+            visited.add((i,j))
             if wordIndex == len(word)-1:
                 self.ans = True
                 return
@@ -12,9 +13,10 @@ class Solution:
                 ni, nj = i+_x, j+_y
                 if 0<=ni<m and 0<=nj<n:
                     if word[wordIndex+1] == board[ni][nj] and (ni, nj) not in visited :
-                        visited.add((i,j))
+                        # visited.add((i,j))
                         dfs(ni,nj,wordIndex+1)
-                        visited.remove((i,j))
+                        # visited.remove((i,j))
+            visited.remove((i,j))
             return         
             
         m, n = len(board), len(board[0])
@@ -22,8 +24,8 @@ class Solution:
         for i in range(m):
             for j in range(n):
                 if board[i][j] == word[0]:
-                    visited.add((i,j))
-                    dfs(i, j, 0)
                     visited = set()
+                    dfs(i, j, 0)
+                    
                     
         return self.ans
