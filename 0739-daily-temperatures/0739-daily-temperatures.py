034759a78818f4096bbf632a04839155c239ed8c
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-02-22 06:34:33 -0600

    Time: 940 ms (20.04%), Space: 33 MB (9.32%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,21 +2,22 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         
         '''
-        monostack: store index instead of temperatures
+        monostack / only store smaller tmp 
         
+        store [tmp, index]        
         '''
+        
         stack = []
-        n = len(temperatures)
-        ans = [0]*len(temperatures)
-        for i in range(n):
-            while stack and temperatures[i] > temperatures[stack[-1]]:
-                index = stack.pop()
-                ans[index] = i-index
-            stack.append(i)
-
-        return ans 
-        
-        
-        
-        
-        
+        res = [0] * len(temperatures)
+        for i, tmp in enumerate(temperatures):
+            if not stack:
+                stack.append([tmp, i])
+            elif stack and tmp < stack[-1][0]:
+                stack.append([tmp, i])
+            else:
+                while stack and tmp > stack[-1][0]:
+                    _ , index = stack.pop()
+                    res[index] = i-index
+                stack.append([tmp, i])
+                
+        return res
