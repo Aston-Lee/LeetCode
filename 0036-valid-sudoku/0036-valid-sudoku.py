ccecd687dffef41c0acae99f1b35184561340a62
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-02-24 02:41:58 -0600

    Time: 110 ms (8.20%), Space: 22.7 MB (5.50%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -6,53 +6,52 @@ class Solution(object):
         :type board: List[List[str]]
         :rtype: bool
         """
+        m, n = len(board), len(board[0])
         
-        def check(idx):
+        ## check col
+        for i in range(m):
             appeared = set()
-            for num in board[idx]:
-                if num == '.':
+            for j in range(n):
+                if  board[i][j] == '.':
                     continue
-                if num in appeared:
+                elif board[i][j] in appeared:
                     return False
-                appeared.add(num)
-            return True
-                
-            
-        def checkblock(i, j):
+                else:
+                    appeared.add(board[i][j])
+                    
+
+        ## check rol
+        board = np.transpose(board)
+        for i in range(m):
+            appeared = set()
+            for j in range(n):
+                if  board[i][j] == '.':
+                    continue
+                elif board[i][j] in appeared:
+                    return False
+                else:
+                    appeared.add(board[i][j])
+        board = np.transpose(board)
+        
+        
+        
+        ## check block
+        def _checkblock(i ,j):
             appeared = set()
             for _x in range(3):
                 for _y in range(3):
-                    if board[i+_x][j+_y] == '.':
+                    if  board[i+_x][j+_y] == '.':
                         continue
-                    if board[i+_x][j+_y] in appeared:
+                    elif board[i+_x][j+_y] in appeared:
                         return False
-                    appeared.add(board[i+_x][j+_y])
+                    else:
+                        appeared.add(board[i+_x][j+_y])
             return True
         
-            
-        for i in range(9):
-            if not check(i):
-                return False
-        
-            
-        for i in range(0,9,3):
-            for j in range(0,9,3):
-                if not checkblock(i,j):
+        for i in range(m/3):
+            for j in range(n/3):
+                if not _checkblock(3*i, 3*j):
                     return False
-           
         
-        board = np.transpose(board)
-        for i in range(9):
-            if not check(i):
-                return False   
-            
         return True
-        
-            
-            
-            
-        
-        
-        
-        
         
