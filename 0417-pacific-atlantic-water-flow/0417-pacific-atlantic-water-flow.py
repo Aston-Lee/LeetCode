586b7bb2385b323cd58d4a66c6f6138f543d0aa7
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-02-25 11:01:12 -0600

    Time: 215 ms (56.30%), Space: 18 MB (89.51%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -2,36 +2,46 @@ class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         
         DIR = [(0,1), (0,-1), (1,0), (-1,0)]
-        
-        def bfs(queue):
-            reachable = set()
-            while queue:
-                i, j = queue.pop()
-                reachable.add((i,j))
-                for x, y in DIR:
-                    col, row = i+x, j+y
-                    if not (0 <= col < m) or not (0 <= row < n):
-                        continue
-                    if (col,row) in reachable:
-                        continue
-                    if heights[col][row] < heights[i][j]:
-                        continue
-                    queue.append((col, row))
-                    
-            return reachable
-        
         m, n = len(heights), len(heights[0])
         
-        pacific, atlantic = deque(), deque()
+        pacific_reachable, atlantic_reachable = set(), set()
+        # pacific_visited, atlantic_visited = set(), set()
+        
+        def bfs(dq):
+            visitedSet = set()
+            reachable = set()
+            while dq:
+                for _ in range(len(dq)):
+                    i, j = dq.popleft()
+                    reachable.add((i,j))
+                    for (_x, _y) in DIR:
+                        ni, nj = i+_x, j+_y
+                        if 0<=ni<m and 0<=nj<n:
+                            if (ni,nj) not in visitedSet and heights[i][j] <= heights[ni][nj]:
+                                dq.append((ni,nj))
+                                visitedSet.add((ni,nj))
+                    # print(reachable)
+            return reachable
+        
+        ## do pacific
+        dq = deque()
         for i in range(m):
-            pacific.append((i,0))
-            atlantic.append((i,n-1))
-            
-        for i in range(n):
-            pacific.append((0,i))
-            atlantic.append((m-1,i))
-            
-        pacificReachable  = bfs(pacific)
-        atlanticReachable = bfs(atlantic)
-            
-        return list(pacificReachable & atlanticReachable)
+            if i == 0:
+                for j in range(n):
+                    dq.append((0,j))
+            else:
+                dq.append((i,0)) 
+        pacific_reachable = bfs(dq)
+        
+        
+        ## do atlantic
+        dq = deque()
+        for i in range(m):
+            if i == m-1:
+                for j in range(n):
+                    dq.append((m-1,j))
+            else:
+                dq.append((i,n-1))
+        atlantic_reachable = bfs(dq)
+        
+        return list(pacific_reachable & atlantic_reachable)
