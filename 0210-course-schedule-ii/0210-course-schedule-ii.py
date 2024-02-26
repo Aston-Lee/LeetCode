3342ff4e1aa697df902ab8c79b90004e9856df60
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-02-26 05:30:18 -0600

    Time: 84 ms (90.11%), Space: 18.5 MB (55.41%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,29 +1,31 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         
-        in_degree = defaultdict(int)
         preMap = defaultdict(list)
+        indegree = defaultdict(int)
+        startNode = set([i for i in range(numCourses)])
         
         for crs, pre in prerequisites:
-            in_degree[pre] += 1
-            preMap[crs].append(pre)
+            preMap[pre].append(crs)
+            indegree[crs] += 1
+            if crs in startNode:
+                startNode.remove(crs)
             
-        start_nodes = deque()
-        for i in range(numCourses):
-            if in_degree[i] == 0:
-                start_nodes.append(i)
+            
+        dq = deque(list(startNode))
         
-        sorted_order = []
-        while start_nodes:
-            crs = start_nodes.popleft()
-            sorted_order.append(crs)
-            for pre in preMap[crs]:
-                in_degree[pre] -= 1
-                if in_degree[pre] == 0:
-                    start_nodes.append(pre)
+        res = []
+        while dq:
+            for _ in range(len(dq)):
+                i = dq.popleft()
+                res.append(i)
+                for crs in preMap[i]:
+                    indegree[crs] -= 1
+                    if indegree[crs] == 0:
+                        dq.append(crs)
+                        
+        return res if len(res) == numCourses else []
+                        
         
-        if len(sorted_order) == numCourses:
-            return sorted_order[::-1]
-        else:
-            return []
-                
+            
+            
