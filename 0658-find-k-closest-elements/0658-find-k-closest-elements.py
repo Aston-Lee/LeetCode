cf6885886dbd723e007eefd5c5e6d5e9daed962f
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-02-28 09:26:57 -0600

    Time: 277 ms (21.39%), Space: 19.5 MB (5.91%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        
+        heap = []
+        
+        for num in arr:
+            if len(heap) < k:
+                heapq.heappush(heap, [-abs(num-x), -num])
+            else:
+                heapq.heappushpop(heap, [-abs(num-x), -num])
+                
+        print(heap)
+        
+        res = []
+        for _, num in heap:
+            res.append(-num)
+
+        return sorted(res)
