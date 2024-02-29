0b0426b057cbc3047187c15077e6a77d2728f8cb
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-02-29 02:48:12 -0600

    Time: 149 ms (83.75%), Space: 37.5 MB (14.41%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -4,37 +4,21 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def sortList(self, head: ListNode) -> ListNode:
-        if not head or not head.next:
-            return head
-        mid = self.getMid(head)
-        left = self.sortList(head)
-        right = self.sortList(mid)
-        return self.merge(left, right)
-
-    def getMid(self, head: ListNode) -> ListNode:
-        slow = head
-        fast = head
-        while fast.next and fast.next.next:
-            slow = slow.next
-            fast = fast.next.next
-        mid = slow.next
-        slow.next = None
-        return mid
-
-    def merge(self, list1: ListNode, list2: ListNode) -> ListNode:
-        dummyHead = ListNode(0)
-        ptr = dummyHead
-        while list1 and list2:
-            if list1.val < list2.val:
-                ptr.next = list1
-                list1 = list1.next
-            else:
-                ptr.next = list2
-                list2 = list2.next
-            ptr = ptr.next
-        if list1:
-            ptr.next = list1
-        else:
-            ptr.next = list2
-        return dummyHead.next
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        
+        # brute force
+        mem = []
+        node = head
+        
+        while node:
+            mem.append(node.val)
+            node = node.next
+           
+        mem.sort()
+        
+        resptr = res = ListNode(0)
+        for num in mem:
+            res.next = ListNode(num)
+            res = res.next
+        
+        return resptr.next
