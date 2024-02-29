37edf5cbbe4756bf416b5176a92d7aad0752a910
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-02-28 22:55:29 -0600

    Time: 374 ms (43.46%), Space: 17.9 MB (72.04%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,18 +1,12 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        ## thinking from the back if the back can reach to front wiil be O(n^2)
         
-        ## 2 pointer, O(n)
-        target = len(nums)-1
-        l, r = 0, 0
-        while(l<r+1):
-            if r >= target:
-                return True
-            if l + nums[l] > r:
-                r = l + nums[l]
-            if l == r and nums[l] == 0:
-                return False
-            l += 1
+        farthest = 0
         
+        for i, num in enumerate(nums):
+            if farthest >= i:
+                farthest = max(farthest, nums[i]+i)  
+                
+        return farthest >= len(nums)-1                
         
         
