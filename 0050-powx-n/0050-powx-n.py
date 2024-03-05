75adb36b3c2db4044796fde7f33f0ca858e0f706
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-03-05 01:17:25 -0600

    Time: 35 ms (58.22%), Space: 16.6 MB (78.85%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,19 +1,19 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
         
-        @cache
-        def fastpow(x, w):
-            if w == 0:
-                return 1
+        def fastPow(x, n):
+            if x == 0: 
+                return 0
+            if n==1:
+                return x
+            res = fastPow(x * x, n // 2)
+            return x * res if n % 2 else res
             
-            if w%2:
-                return x* fastpow(x, w//2) * fastpow(x, w//2)
-            else:
-                return fastpow(x, w//2) * fastpow(x, w//2)
-        
-        
+            
         if n < 0:
-            n = -n
             x = 1/x
-
-        return fastpow(x, n)
+            n = -n
+        if n == 0:
+            return 1
+        
+        return fastPow(x, n)
