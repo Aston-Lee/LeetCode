ffede222db8f1d077ef4c45ce5a4eb2d5dba2575
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-03-05 01:37:06 -0600

    Time: 50 ms (26.99%), Space: 17 MB (98.66%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,25 +1,44 @@
-class Solution(object):
-    def searchMatrix(self, matrix, target):
-        """
-        :type matrix: List[List[int]]
-        :type target: int
-        :rtype: bool
-        """
-        def dfs(i, l, r):
-            while(l<=r):
-                mid = (l+r) // 2
-                if matrix[i][mid] == target:
-                    return True
-                elif matrix[i][mid] < target:
-                    l = mid + 1
-                else:
-                    r = mid - 1
-            return False
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
-        for i in range(len(matrix)):
-            if matrix[i][0] == target or target == matrix[i][-1]:
-                return True
-            elif matrix[i][0] < target and target < matrix[i][-1]:
-                return dfs(i, 0, len(matrix[i]))
-            
+        # decide col
+        
+        m, n = len(matrix), len(matrix[0])
+        
+        max_element = matrix[-1][-1] + 1
+        matrix.append([max_element] * n)
+        # print(matrix)
+        
+        
+        l, r = 0, m-1
+        index = None
+        while l <= r:
+            mid = (l+r)//2
+            # print(l, r, mid)
+            if matrix[mid][0] <= target < matrix[mid+1][0]:
+                index = mid 
+                break
                 
+            if matrix[mid][0] <= target and matrix[mid+1][0] <= target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        
+        # print(index)
+        if index == None:
+            return False 
+                
+        l, r = 0, n-1
+        while l <= r:  
+            mid = (l+r) // 2
+            if matrix[index][mid] == target:
+                return True
+            
+            if matrix[index][mid] > target:
+                r = mid - 1
+            else:
+                l = mid + 1
+            
+            
+        return False 
+        
