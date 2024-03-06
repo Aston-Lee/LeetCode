f5483725631925094125abe44dfa95eeabd511ed
Aston <76436175+Aston-Lee@users.noreply.github.com>
2024-03-06 01:47:34 -0600

    Time: 107 ms (43.61%), Space: 18.1 MB (43.27%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,15 +1,39 @@
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
-        ## bellman ford - shortest path
-        prices = [float('inf')] * n
-        prices[src] = 0
         
-        for i in range(k+1):
-            tmpPrices = prices.copy()
+        
+        # try dq
+        adj = defaultdict(list)
+        for start, end, price in flights:
+            adj[start].append([price, end])
             
-            for s, d, p in flights:
-                if prices[s] == float(inf):
-                    continue
-                tmpPrices[d] = min(prices[s]+p, tmpPrices[d])
-            prices = tmpPrices
-        return -1 if prices[dst] == float("inf") else prices[dst]
+        res = float('inf')
+        dq = deque()
+        stops = 0
+        for price, station in adj[src]:
+            if station == dst:
+                res = min(res, price)
+            else:
+                dq.append([price, station])
+        adj[src] = []
+        
+        visited = defaultdict(int)
+        
+        while dq and stops < k:
+            stops += 1
+            for _ in range(len(dq)):
+                price, end = dq.popleft()
+                for add_price, end_station in adj[end]:
+                    new_price = price + add_price 
+                    if end_station == dst:
+                        res = min(res, new_price)
+                    else:
+                        if end_station in visited and  visited[end_station] < new_price:
+                            continue
+                        else:
+                            dq.append([new_price, end_station])
+                            visited[end_station] = new_price
+                        
+        return res if res != float('inf') else -1
+        
+            
